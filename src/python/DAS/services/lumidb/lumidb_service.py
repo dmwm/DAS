14d8291e938baa75f0d07a766c0c2c5113df96e7
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-03 16:38:25 +0000

    Added more parsers for lumidb APIs
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@84 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/services/lumidb/lumidb_service.py
+++ src/python/DAS/services/lumidb/lumidb_service.py
@@ -2,15 +2,15 @@
 """
 LumiDB service
 """
-__revision__ = "$Id: lumidb_service.py,v 1.1 2009/04/07 20:10:09 valya Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: lumidb_service.py,v 1.2 2009/04/08 20:54:03 valya Exp $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "Valentin Kuznetsov"
 
 import re
 import types
 
 from DAS.services.abstract_service import DASAbstractService
-from DAS.utils.utils import map_validator
+from DAS.utils.utils import map_validator, query_params
 
 class LumiDBService(DASAbstractService):
     def __init__(self, config):
@@ -69,20 +69,178 @@ class LumiDBService(DASAbstractService):
         and return results in a list with provided row.
         Return: [{'key':'value', 'key2':'value2'}, ...]
         """
-        print "CALL lumidb::api"
-        print query
-        print cond_dict
-        condlist = query.split(' where ')
-        cond = query.split(' where ')[-1]
-        cond_key, cond_val = cond.split()[0].split("=")
-        params = {'api':'findIntRawLumi', 'run_number' : int(cond_val)}
-        params = {'api':'findTrgPathByRun', 'run_number' : int(cond_val)}
-        print params
-        # construct params out of provided query and cond_dict
-        data  = self.getdata(self.url, params)
-        print data
-        import sys
-        sys.exit(0)
-        # parse output data here
+        selkeys, cond = query_params(query)
+        params = {}
+        for key in cond.keys():
+            oper, val = cond[key]
+            if  oper == '=':
+                params[key] = val
+            else:
+                raise Exception("DAS::%s, not supported operator '%s'" \
+                % (self.name, oper))
+        if  params.has_key('run'): # LumiDB uses run_number
+            val = params['run']
+            params['run_number'] = val
+            del params['run']
+        for key in cond_dict: # LumiDB uses run_number rather then run key
+            if  key == 'run':
+                name = 'run_number'
+            else:
+                name = key
+            params[name] = cond_dict[key]
+
+        apiname = ""
+        args = {}
+        # check if all requested keys are covered by one API
+        for api, aparams in self.map.items():
+            if  set(selkeys) & set(aparams['keys']) == set(selkeys):
+                apiname = api
+                for par in aparams['params']:
+                    if  params.has_key(par):
+                        args[par] = params[par]
+                args['api'] = api
+                res = self.getdata(self.url, args)
+                jsondict = eval(res)
+                if  jsondict.has_key('error'):
+                    continue
+                data = getattr(self, 'parser_%s' % apiname)(jsondict)
+                return data
+
+        # if one API doesn't cover sel keys, will call multiple APIs
+        apidict = {}
+        for key in selkeys:
+            for api, aparams in self.map.items():
+                if  aparams['keys'].count(key) and not apidict.has_key(api):
+                    args = {}
+                    for par in aparams['params']:
+                        if  params.has_key(par):
+                            args[par] = params[par]
+                    apidict[api] = args
+        rel_keys = []
+        resdict  = {}
+        for api, args in apidict.items():
+            args['api'] = api
+            res = self.getdata(self.url, args)
+            jsondict = eval(res)
+            data = getattr(self, 'parser_%s' % api)(jsondict)
+            resdict[api] = data
+            first_row = data[0]
+            keys = first_row.keys()
+            if  not rel_keys:
+                rel_keys = set(list(keys))
+            else:
+                rel_keys = rel_keys & set(keys)
+        data = self.product(resdict, rel_keys)
+        return data
+
+
+    def parser_findTrgPathByRun(self, jsondict):
+        """
+        Parser of output for findTrgPathByRun lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'trigpath':val}
+            data.append(row)
+        return data
+
+    def parser_findIntegratedLuminosity(self, jsondict):
+        """
+        Parser of output for findIntegratedLuminosity lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'intlumi':val}
+            data.append(row)
+        return data
+
+    def parser_findAvgIntegratedLuminosity(self, jsondict):
+        """
+        Parser of output for findAvgIntegratedLuminosity lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'avglumi':val}
+            data.append(row)
+        return data
+
+    def parser_findIntRawLumi(self, jsondict):
+        """
+        Parser of output for findIntRawLumi lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'intrawlumi':val}
+            data.append(row)
+        return data
+
+    def parser_findL1Counts(self, jsondict):
+        """
+        Parser of output for findL1Counts lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'L1counts':val}
+            data.append(row)
+        return data
+
+    def parser_findHLTCounts(self, jsondict):
+        """
+        Parser of output for findHLTCounts lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'HLTcounts':val}
+            data.append(row)
+        return data
+
+    def parser_findRawLumi(self, jsondict):
+        """
+        Parser of output for findRawLumi lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'rawlumi':val}
+            data.append(row)
+        return data
+
+    def parser_listLumiByBunch(self, jsondict):
+        """
+        Parser of output for listLumiByBunch lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'lumibybunch':val}
+            data.append(row)
+        return data
+
+    def parser_listLumiSummary(self, jsondict):
+        """
+        Parser of output for listLumiSummary lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'lumisummary':val}
+            data.append(row)
+        return data
+
+    def parser_listLumiTrigger(self, jsondict):
+        """
+        Parser of output for listLumiTrigger lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'lumitrigger':val}
+            data.append(row)
+        return data
+
+    def parser_listLumiDeadTime(self, jsondict):
+        """
+        Parser of output for listLumiDeadTime lumidb API
+        """
+        data = []
+        for key, val in jsondict.items():
+            row = {'lumideadtime':val}
+            data.append(row)
         return data
 
