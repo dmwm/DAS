f5da04c70b21cdcebdb19fb45f01bc83ebe32916
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-03 16:20:24 +0000

    init
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@31 4525493e-7705-40b1-a816-d608a930855b

---
+++ __init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+
+__revision__ = "$Id: __init__.py,v 1.1 2009/01/23 21:47:41 valya Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "Valentin Kuznetsov"
