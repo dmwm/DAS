dcbdc47432cce7233c75144dc4ca653caf448723
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-03 16:41:09 +0000

    Unit test for jsonparser
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@91 4525493e-7705-40b1-a816-d608a930855b

--- test/das_mapping_t.py
+++ test/das_mapping_t.py
@@ -6,17 +6,36 @@ unit test for DAS mapping set of tools
 """
 
 import unittest
-from DAS.core.das_mapping import translate
+from DAS.core.das_mapping import translate, jsonparser
 
 class testDASMapping(unittest.TestCase):
     """
     A test class for the DAS mapping
     """
-    def test_mapconfig(self):                          
+    def test_mapconfig(self): 
         """test DAS translate routine"""
         result = translate('phedex', 'site')
         expect = ['se']
         self.assertEqual(expect, result)
+
+    def test_jsonparser(self): 
+        """test DAS jsonparser routine"""
+        rep1 = {'bytes':1, 'files':1, 'se':'T2'}
+        rep2 = {'bytes':1, 'files':1, 'se':'T3'}
+        rec1 = {'bytes':1, 'files':1, 'name':'ABC', 'replica':rep1}
+        rec2 = {'bytes':2, 'files':2, 'name':'CDE', 'replica':[rep1, rep2]}
+        jsondict = {'service': {'block': [rec1, rec2]},'timestamp':123}
+        result = jsonparser(jsondict, 'block')
+        expect = [rec1, rec2]
+        self.assertEqual(expect, result)
+
+        result = jsonparser(jsondict, 'block.bytes')
+        expect = [1, 2]
+        self.assertEqual(expect, result)
+
+#        result = jsonparser(jsondict, 'block.replica')
+#        expect = [1, 2]
+#        self.assertEqual(expect, result)
 
 #
 # main
