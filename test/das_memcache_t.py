2fff67034ab8710faa555e98490da02ef29b0b2c
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-03 16:22:33 +0000

    Added DAS Cache based on memcached. Modified DAS web to use cache manager and fall back to core API if it's not presented. Memcache works transparently if it is dead DASCache just provide access to DAS core calls
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@37 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/das_memcache_t.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#pylint: disable-msg=C0301,C0103
+
+"""
+Unit test for DAS cache module
+"""
+
+import unittest
+from core.das_memcache import DASCache
+from core.das_core import DASCore
+
+class testDASCache(unittest.TestCase):
+    """
+    A test class for the DAS cache module
+    """
+    def setUp(self):
+        """
+        set up DAS core module
+        """
+        cachelist = ['127.0.0.1:11211']
+        self.dascache = DASCache(cachelist, debug=1)
+        self.das = DASCore()
+
+    def test_key(self):                          
+        """test DAS cache key generator"""
+        query  = "find dataset,admin,node where site=T2_UK_SGrid_Bristol"
+        result = self.dascache.key(query)
+        import md5
+        hash = md5.new()
+        hash.update(query)
+        expect = hash.hexdigest()
+        self.assertEqual(expect, result)
+
+    def test_result(self):                          
+        """test DAS cache result method"""
+        query  = "find dataset,admin,node where site=T2_UK_SGrid_Bristol"
+        result = self.dascache.result(query)
+        expect = self.das.result(query)
+        self.assertEqual(expect, result)
+#
+# main
+#
+if __name__ == '__main__':
+    unittest.main()
