5ee679836c8f0195e405870a4beae500a4b24379
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:40:52 +0000

    Update
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@1167 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/__init__.py
+++ src/python/DAS/__init__.py
@@ -8,8 +8,8 @@ DAS modules
 .. moduleauthor:: Valentin Kuznetsov <vkuznet@gmail.com>
 """
 
-__revision__ = "$Id: __init__.py,v 1.5 2010/04/13 15:15:52 valya Exp $"
-__version__ = "$Revision: 1.5 $"
+__revision__ = "$Id: __init__.py,v 1.6 2010/04/15 15:19:46 valya Exp $"
+__version__ = "$Revision: 1.6 $"
 __author__ = "Valentin Kuznetsov"
 
-version = "V04.00.08"
+version = "0.4.8"
