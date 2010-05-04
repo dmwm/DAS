bd3a372a1bee48c85342f062fefa824229bb4987
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:36:26 +0000

    Made a new unit test which test DASCore workflow and results based on persistent data-services from Google and IP locator
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@1064 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/das_core_t.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#pylint: disable-msg=C0301,C0103
+
+"""
+Unit test for DAS core module
+"""
+
+import os
+import types
+import unittest
+from DAS.utils.das_config import das_readconfig
+from DAS.core.das_core import DASCore
+from DAS.utils.utils import dotdict
+
+class testDASCore(unittest.TestCase):
+    """
+    A test class for the DAS core module
+    """
+    def setUp(self):
+        """
+        set up DAS core module
+        """
+        debug = 0
+        self.das = DASCore(debug=debug)
+
+    def testAddressService(self):
+        """test DASCore with as address service, via google_maps"""
+        # test DAS workflow
+        query  = "city=Ithaca"
+        query  = self.das.adjust_query(query)
+        result = self.das.call(query)
+        expect = 1
+        self.assertEqual(expect, result)
+
+        # test results
+        query  = "city=Ithaca | grep city.Placemark.address"
+        query  = self.das.adjust_query(query)
+        result = self.das.get_from_cache(query)
+        result = [r for r in result][0]
+        result = dotdict(result)._get('city.Placemark.address')
+        expect = 'Ithaca, NY, USA'
+        self.assertEqual(expect, result)
+
+    def testIPService(self):
+        """test DASCore with IP service"""
+        # test DAS workflow
+        query  = "ip=137.138.141.145"
+        query  = self.das.adjust_query(query)
+        result = self.das.call(query)
+        expect = 1
+        self.assertEqual(expect, result)
+
+        # test results
+        query  = "ip=137.138.141.145 | grep ip.City"
+        query  = self.das.adjust_query(query)
+        result = self.das.get_from_cache(query)
+        result = [r for r in result][0]
+        result = dotdict(result)._get('ip.City')
+        expect = 'Geneva'
+        self.assertEqual(expect, result)
+
+#
+# main
+#
+if __name__ == '__main__':
+    unittest.main()
+
+
