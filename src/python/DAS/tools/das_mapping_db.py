c0fe42570f4297b6cbae6777aa46c4c72cf21c92
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:25:46 +0000

    Change mapping tool to handle add/delete maps; ability to add api/notation/presentation maps, etc.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@814 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/tools/das_mapping_db.py
+++ src/python/DAS/tools/das_mapping_db.py
@@ -4,25 +4,15 @@
 """
 DAS command line interface
 """
-__revision__ = "$Id: das_mapping_db.py,v 1.20 2010/01/27 16:53:57 valya Exp $"
-__version__ = "$Revision: 1.20 $"
+__revision__ = "$Id: das_mapping_db.py,v 1.21 2010/02/02 20:12:12 valya Exp $"
+__version__ = "$Revision: 1.21 $"
 __author__ = "Valentin Kuznetsov"
 
-import os
 import sys
 from optparse import OptionParser
-from DAS.core.das_core import DASCore
-from DAS.utils.utils import dump
 from DAS.core.das_mapping_db import DASMapping
 from DAS.utils.logger import DASLogger
-from DAS.services.map_reader import read_api_map
-from DAS.services.map_reader import read_notation_map
-from DAS.services.map_reader import read_presentation_map
-from DAS.utils.das_config import ACTIVE_SYSTEMS
-
-import sys
-if sys.version_info < (2, 6):
-    raise Exception("DAS requires python 2.6 or greater")
+from DAS.services.map_reader import read_service_map
 
 class DASOptionParser: 
     """
@@ -34,9 +24,6 @@ class DASOptionParser:
                                           type="int", default=None, 
                                           dest="debug",
              help="verbose output")
-#        self.parser.add_option("--db", action="store", type="string", 
-#             default='das_mapping.db', dest="dbfile",
-#             help="specify DB file to use.")
         self.parser.add_option("--host", action="store", type="string",
              default="localhost", dest="host", help="specify MongoDB hostname")
         self.parser.add_option("--port", action="store", type="int",
@@ -45,10 +32,21 @@ class DASOptionParser:
              default="mapping" , dest="db", help="specify MongoDB db name")
         self.parser.add_option("--system", action="store", type="string",
              default=None , dest="system", help="specify DAS sub-system")
+        self.parser.add_option("--api-map", action="store", type="string",
+             default=None , dest="amap", help="specify api map file")
+        self.parser.add_option("--notation-map", action="store", type="string",
+             default=None , dest="nmap", help="specify notation map file")
+        self.parser.add_option("--presentation-map", action="store", type="string",
+             default=None , dest="pmap", help="specify presentation map file")
         self.parser.add_option("--list-apis", action="store_true", 
              dest="listapis", help="return a list of APIs")
         self.parser.add_option("--list-daskeys", action="store_true", 
              dest="listkeys", help="return a list of DAS keys")
+        self.parser.add_option("--remove", action="store", type="string",
+             default=None , dest="remove", 
+             help="remove DAS Mapping DB record, provide the spec")
+        self.parser.add_option("--clean", action="store_true", 
+             dest="clean", help="clean DAS mapping DB")
     def getOpt(self):
         """
         Returns parse list of options
@@ -57,8 +55,8 @@ class DASOptionParser:
 
 def nooutput(results):
     """Just iterate over generator, but don't print it out"""
-    for i in results:
-        a = 1
+    for row in results:
+        pass
 #
 # main
 #
@@ -68,7 +66,8 @@ if __name__ == '__main__':
 
     logger = DASLogger(verbose=opts.debug, stdout=opts.debug)
     config = dict(logger=logger, verbose=opts.debug,
-        mapping_dbhost=opts.host, mapping_dbport=opts.port, mapping_dbname=opts.db)
+        mapping_dbhost=opts.host, mapping_dbport=opts.port, 
+        mapping_dbname=opts.db)
 
     mgr = DASMapping(config)
 
@@ -78,39 +77,41 @@ if __name__ == '__main__':
         sys.exit(0)
 
     if  opts.listkeys:
-        keys = mgr.list_daskeys(opts.system)
+        keys = mgr.daskeys(opts.system)
         print keys
         sys.exit(0)
-    # daskeys defines a mapping between daskey used in DAS-QL and DAS record key
-    # e.g. block means block.name, the pattern defines regular expression to which
-    # daskey should satisfy
-    #
-    # api2das defines mapping between API input parameter and DAS-QL key,
-    # dict(api_param='storage_element_name', das_key='site', 
-    #      pattern="re.compile('([a-zA-Z0-9]+\.){2}')"),
-    # here pattern is applied to passed api_param
 
-    # clean-up existing Mapping DB
-    mgr.delete_db()
-    mgr.create_db()
-
-    # read API, notation maps for all known data-services
-    services = ACTIVE_SYSTEMS.split(',')
-    for srv in services:
-        print "\nAdding API map for %s system" % srv
-        for rec in read_api_map(srv):
+    if  opts.amap:
+        for rec in read_service_map(opts.amap, field='api'):
             if  opts.debug:
                 print rec
+            spec = dict(rec)
+            spec.pop('created')
+            mgr.remove(spec) # remove previous record
             mgr.add(rec)
-        print "Adding notation map for %s system" % srv
-        rec = read_notation_map(srv)
-        if  opts.debug:
-            print rec
-        mgr.add(rec)
-    print "\nAdding presentation map for all systems"
-    rec = read_presentation_map()
-    if  opts.debug:
-        print rec
-    mgr.add(rec)
- 
-    print "New DAS Mapping DB has been created"
+
+    if  opts.nmap:
+        for rec in read_service_map(opts.nmap, field='notations'):
+            if  opts.debug:
+                print rec
+            spec = dict(rec)
+            spec.pop('created')
+            mgr.remove(spec) # remove previous record
+            mgr.add(rec)
+
+    if  opts.pmap:
+        # clean first
+        for rec in read_service_map(opts.pmap, field='presentation'):
+            if  opts.debug:
+                print rec
+            spec = dict(rec)
+            spec.pop('created')
+            mgr.remove(spec) # remove previous record
+            mgr.add(rec)
+
+    if  opts.clean:
+        mgr.delete_db()
+        mgr.create_db()
+
+    if  opts.remove:
+        mgr.remove(opts.remove)
