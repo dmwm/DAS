88e62a2078ac6e606100fc627824f7bef11fd725
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:07:51 +0000

    Finished code migration to use persistent storage, DB.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@379 4525493e-7705-40b1-a816-d608a930855b

--- test/das_viewmanager_t.py
+++ test/das_viewmanager_t.py
@@ -5,8 +5,9 @@
 Unit test for DAS view manager
 """
 
+import os
 import unittest
-from DAS.core.das_viewmanager import DASViewManager
+from DAS.core.das_viewmanager import DASViewManager, strip_query
 from DAS.utils.das_config import das_readconfig
 from DAS.utils.logger import DASLogger
 
@@ -19,13 +20,14 @@ class testDAS(unittest.TestCase):
         """
         set up DAS core module
         """
+        self.db  = 'test_views.db'
+        self.tearDown()
         debug    = 0
         config   = das_readconfig()
         logger   = DASLogger(verbose=debug, stdout=debug)
         config['logger']  = logger
         config['verbose'] = debug
-        self.db = 'test_views.db'
-        config['filecache_db_engine'] = 'sqlite:///%s' % self.db
+        config['views_engine'] = 'sqlite:///%s' % self.db
         self.view = DASViewManager(config)
 
     def tearDown(self):
@@ -36,7 +38,7 @@ class testDAS(unittest.TestCase):
             pass
 
     def test_view1(self):                          
-        """test DAS view creation"""
+        """test view creation"""
         expect = 'find dataset, count(file)'
         query  = expect + ' where bla'
         self.view.create('dataset', query)
@@ -44,22 +46,37 @@ class testDAS(unittest.TestCase):
         self.assertEqual(expect, result)
 
     def test_view2(self):                          
-        """test DAS view get"""
+        """test view get"""
         self.assertRaises(Exception, self.view.get, 'lll')
 
     def test_view3(self):                          
-        """test DAS view update"""
-        query0 = 'find dataset, count(file)'
+        """test to check that we add the same view twice"""
+        query = 'find dataset, count(file), run'
+        self.assertRaises(Exception, self.view.create, ('dataset', query))
+
+    def test_view4(self):
+        """test view update"""
+        query0 = 'find dataset, count(file), run'
         expect = 'find dataset, count(file), sum(file.size)'
         query  = expect + ' where bla'
         # first we should get exception that view doesn't exists
         self.assertRaises(Exception, self.view.update, 'dataset')
 
-        self.view.create('dataset', query0)
-        self.view.update('dataset', query)
-        result = self.view.get('dataset')
+        self.view.create('dataset_test', query0)
+        self.view.update('dataset_test', query)
+        result = self.view.get('dataset_test')
         self.assertEqual(expect, result)
 
+    def test_view5(self):
+        """test to checkout all views"""
+        query1 = u'find dataset, count(file) where bla'
+        self.view.create('dataset1', query1)
+        query2 = u'find dataset, count(file), sum(file) where bla'
+        self.view.create('dataset2', query2)
+        result = self.view.all()
+        expect = {u'dataset1': strip_query(query1), 
+                  u'dataset2': strip_query(query2)}
+        self.assertEqual(expect, result)
 
 #
 # main
