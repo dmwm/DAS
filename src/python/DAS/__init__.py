d5a5490c77d433162c18db14045b5f8f8c777bdc
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 23:44:21 +0000

    Adjust docstrings to sphinx syntax
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@1883 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/__init__.py
+++ src/python/DAS/__init__.py
@@ -2,9 +2,11 @@
 #-*- coding: ISO-8859-1 -*-
 
 """
-main DAS module
+DAS modules
+
+.. moduleauthor:: Valentin Kuznetsov <vkuznet@gmail.com>
 """
 
-__revision__ = "$Id: __init__.py,v 1.1 2009/03/09 19:55:00 valya Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: __init__.py,v 1.2 2010/01/25 20:19:15 valya Exp $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "Valentin Kuznetsov"
--- src/python/DAS/utils/__init__.py
+++ src/python/DAS/utils/__init__.py
@@ -2,9 +2,11 @@
 #-*- coding: ISO-8859-1 -*-
 
 """
-DAS utils module
+DAS utilities
+
+.. moduleauthor:: Valentin Kuznetsov <vkuznet@gmail.com>
 """
 
-__revision__ = "$Id: __init__.py,v 1.1 2009/03/09 19:43:35 valya Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: __init__.py,v 1.2 2010/01/25 20:18:16 valya Exp $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "Valentin Kuznetsov"
