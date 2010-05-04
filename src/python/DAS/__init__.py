1aa635f7458281827152bcb7fef4b9ae6747dba4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:42:33 +0000

    Stupid CVS can't tag by numbers, it requires tag to start with letter
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@1207 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/__init__.py
+++ src/python/DAS/__init__.py
@@ -8,8 +8,8 @@ DAS modules
 .. moduleauthor:: Valentin Kuznetsov <vkuznet@gmail.com>
 """
 
-__revision__ = "$Id: __init__.py,v 1.7 2010/04/30 16:45:54 valya Exp $"
-__version__ = "$Revision: 1.7 $"
+__revision__ = "$Id: __init__.py,v 1.8 2010/04/30 17:14:35 valya Exp $"
+__version__ = "$Revision: 1.8 $"
 __author__ = "Valentin Kuznetsov"
 
-version = "0.4.9"
+version = "V0.4.9"
