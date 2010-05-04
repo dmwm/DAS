1a94ad906cbcb867093a019f5b0f529cc4f4b8b3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:03:26 +0000

    Removed das_cache_client.py since it has wrong permission, will put it back after this commit
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@269 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/DAS/tools/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+
+"""
+DAS tools
+"""
+
+__revision__ = "$Id: __init__.py,v 1.1 2009/05/28 19:22:54 valya Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "Valentin Kuznetsov"
