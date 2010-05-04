dd174e4432de280725ecf57de75631f80f426c2f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:41:45 +0000

    Added reserved/special DAS keys
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@1188 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/core/das_ql.py
+++ src/python/DAS/core/das_ql.py
@@ -6,16 +6,19 @@ DAS QL module include definition of DAS operators,
 filters, aggregators, etc.
 """
 
-__revision__ = "$Id: das_ql.py,v 1.3 2010/03/09 02:31:19 valya Exp $"
-__version__ = "$Revision: 1.3 $"
+__revision__ = "$Id: das_ql.py,v 1.4 2010/04/30 16:35:03 valya Exp $"
+__version__ = "$Revision: 1.4 $"
 __author__ = "Valentin Kuznetsov"
 
 import inspect
 from DAS.core.das_aggregators import ResultObject
 
 DAS_FILTERS   = ['grep']
-DAS_OPERATORS = ['!=', '<=', '<', '>=', '>', '=', 
-                 'between', 'nin', 'in', 'last']
+DAS_OPERATORS = ['=', 'between', 'in', 'last']
+#DAS_OPERATORS = ['!=', '<=', '<', '>=', '>', '=', 
+#                 'between', 'nin', 'in', 'last']
+DAS_SPECIALS  = ['date', 'system']
+DAS_RESERVED  = DAS_FILTERS + DAS_OPERATORS + DAS_SPECIALS
 URL_MAP       = {
     '!=' : '%21%3D',
     '<=' : '%3C%3D',
@@ -33,7 +36,7 @@ MONGO_MAP     = {
     'in' : '$in',
     '!=' : '$ne',
     'nin': '$nin',
-    'between':'$in',
+#    'between':'$in',
 }
 def mongo_operator(das_operator):
     """
@@ -54,6 +57,18 @@ def das_operators():
     Return list of DAS operators
     """
     return DAS_OPERATORS
+
+def das_special_keys():
+    """
+    Return list of DAS special keywords
+    """
+    return DAS_SPECIALS
+
+def das_reserved():
+    """
+    Return list of DAS reserved keywords
+    """
+    return DAS_RESERVED
 
 def das_aggregators():
     """
