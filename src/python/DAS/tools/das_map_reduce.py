2dc7519fd9dac095db003609c8a18ab92f65b7a3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 23:40:40 +0000

    Tools to manipulate map/reduce functions in DAS. All map/reduce code will be stored into das.mapreduce collection such that we can use it in DAS QL
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@1798 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/DAS/tools/das_map_reduce.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+#pylint: disable-msg=C0301,C0103
+
+"""
+DAS admin tool to handle DAS map/reduce records.
+"""
+__revision__ = "$Id"
+__version__ = "$Revision"
+__author__ = "Valentin Kuznetsov"
+
+import sys
+import time
+import types
+import traceback
+from optparse import OptionParser
+from DAS.utils.das_config import das_readconfig
+
+# monogo db modules
+from pymongo.connection import Connection
+from pymongo import DESCENDING
+
+class DASOptionParser: 
+    """
+     DAS cli option parser
+    """
+    def __init__(self):
+        self.parser = OptionParser()
+        self.parser.add_option("--host", action="store", type="string", 
+             default="localhost", dest="host",
+             help="specify MongoDB hostname")
+        self.parser.add_option("--port", action="store", type="int", 
+             default="27017", dest="port",
+             help="specify MongoDB port")
+        self.parser.add_option("--list", action="store", dest="list",
+             default="", type="string",
+             help="list map/reduce functions in DAS, accept alias name")
+        self.parser.add_option("--delete", action="store", dest="delete",
+             default="", type="string",
+             help="delete map/reduce functions in DAS, accept alias name")
+
+    def get_opt(self):
+        """
+        Returns parse list of options
+        """
+        return self.parser.parse_args()
+
+class MapReduceMgr(object):
+    """
+    General class to work with DAS mongo dbs.
+    """
+    def __init__(self, host, port):
+        self.host   = host
+        self.port   = port
+        self.conn   = Connection(host, port)
+        self.mapreduce = self.conn['das']['mapreduce']
+
+    def add_mapreduce(self, name, fmap, freduce):
+        """
+        Add mapreduce record and assign it to given name.
+        """
+        print "Add %s map/reduce function" % name
+        exists = self.mapreduce.find_one({'name':name})
+        if  exists:
+            raise Exception('Map/reduce functions for %s already exists' % name)
+        self.mapreduce.insert(dict(name=name, map=fmap, reduce=freduce))
+        self.mapreduce.ensure_index([('name', DESCENDING)])
+
+    def list_mapreduce(self, name=None):
+        """
+        List existing mapreduce records for given name
+        """
+        if  name and name != '*':
+            result = self.mapreduce.find({'name':name})
+        else:
+            result = self.mapreduce.find({})
+        for row in result:
+            yield row
+
+    def delete_mapreduce(self, name=None):
+        """
+        Delete existing mapreduce records for given name
+        """
+        print "Delete %s map/reduce function(s)" % name
+        if  name and name != '*':
+            self.mapreduce.remove({'name':name})
+        else:
+            self.mapreduce.remove({})
+#
+# main
+#
+if __name__ == '__main__':
+    OMGR = DASOptionParser()
+    (opts, args) = OMGR.get_opt()
+    MGR = MapReduceMgr(opts.host, opts.port)
+
+    delete_name = opts.delete
+    if  delete_name:
+        MGR.delete_mapreduce(delete_name)
+        sys.exit(0)
+
+    list_name = opts.list
+    if  list_name:
+        for row in MGR.list_mapreduce(list_name):
+            print row
+        sys.exit(0)
+
+    # delete all existing records
+    MGR.delete_mapreduce('*')
+
+    # add sum(block.replica.size) map/reduce function
+    MAP = """
+function() {
+if(this.block) {
+    if (this.block.replica) {
+        if (this.block.replica.size) emit(this.block.replica.size, 1);
+    }
+}
+}
+"""
+    REDUCE = """
+function (key, values) {
+    var total = 0;
+    for (var i = 0; i < values.length; i++) {
+        total += values[i];
+    }
+    return total;
+"""   
+    MGR.add_mapreduce('sum(block.replica.size)', MAP, REDUCE)
+
+
