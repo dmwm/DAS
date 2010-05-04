f4ffaf26c70209f2424e99660312d0191425e0b4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 23:57:59 +0000

    Adjust code to use new DAS configuration dict. Get rid off getarg.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@2194 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/core/das_analytics_db.py
+++ src/python/DAS/core/das_analytics_db.py
@@ -7,22 +7,20 @@ DAS analytics DB module.
 
 from __future__ import with_statement
 
-__revision__ = "$Id: das_analytics_db.py,v 1.16 2010/03/05 18:09:26 valya Exp $"
-__version__ = "$Revision: 1.16 $"
+__revision__ = "$Id: das_analytics_db.py,v 1.17 2010/04/05 19:11:44 valya Exp $"
+__version__ = "$Revision: 1.17 $"
 __author__ = "Valentin Kuznetsov"
 
-import os
+# system modules
 import types
-import traceback
-import DAS.utils.jsonwrapper as json
 
 # monogo db modules
 from pymongo.connection import Connection
 from pymongo import DESCENDING
 
 # DAS modules
-from DAS.utils.utils import getarg, gen2list, genkey
-from DAS.core.das_mongocache import encode_mongo_query, decode_mongo_query
+from DAS.utils.utils import gen2list, genkey
+from DAS.core.das_mongocache import encode_mongo_query
 
 class DASAnalytics(object):
     """
@@ -31,24 +29,18 @@ class DASAnalytics(object):
     def __init__(self, config):
         self.logger  = config['logger']
         self.verbose = config['verbose']
-        self.dbhost  = config['analytics_dbhost']
-        self.dbport  = config['analytics_dbport']
-        self.dbname  = getarg(config, 'analytics_dbname', 'analytics')
+        self.dbhost  = config['analyticsdb']['dbhost']
+        self.dbport  = config['analyticsdb']['dbport']
+        self.dbname  = config['analyticsdb'].\
+                        get('analytics_dbname', 'analytics')
         self.colname = 'db'
 
         msg = "DASAnalytics::__init__ %s:%s@%s" \
         % (self.dbhost, self.dbport, self.dbname)
         self.logger.info(msg)
-        
-        self.create_db()
-
-    def create_db(self):
-        """
-        Establish connection to MongoDB back-end and create DB.
-        """
         self.conn    = Connection(self.dbhost, self.dbport)
-        self.db      = self.conn[self.dbname]
-        self.col     = self.db[self.colname]
+        database     = self.conn[self.dbname]
+        self.col     = database[self.colname]
 
     def delete_db(self):
         """
@@ -61,13 +53,13 @@ class DASAnalytics(object):
         Add DAS-QL/MongoDB-QL queries into analytics.
         """
         if  type(mongoquery) is types.DictType:
-#            mongoquery = json.dumps(mongoquery)
             mongoquery = encode_mongo_query(mongoquery)
         msg = 'DASAnalytics::add_query("%s", %s)' % (dasquery, mongoquery)
         self.logger.info(msg)
         dhash   = genkey(dasquery)
         qhash   = genkey(mongoquery)
-        record  = dict(dasquery=dasquery, mongoquery=mongoquery, qhash=qhash, dhash=dhash)
+        record  = dict(dasquery=dasquery, mongoquery=mongoquery, 
+                        qhash=qhash, dhash=dhash)
         if  self.col.find({'qhash':qhash}).count():
             return
         self.col.insert(record)
@@ -81,7 +73,6 @@ class DASAnalytics(object):
         """
         orig_query = query
         if  type(query) is types.DictType:
-#            query = json.dumps(query)
             query = encode_mongo_query(query)
         msg = 'DASAnalytics::add_api(%s, %s, %s, %s)' \
         % (system, query, api, args)
@@ -109,20 +100,12 @@ class DASAnalytics(object):
         Update records for given system/query.
         """
         if  type(query) is types.DictType:
-#            query = json.dumps(query)
             query = encode_mongo_query(query)
         msg = 'DASAnalytics::update(%s, %s)' % (system, query)
         self.logger.info(msg)
         qhash = genkey(query)
         cond  = {'qhash':qhash, 'system':system}
-        # TODO:
-        # MongoDB has a bug, http://jira.mongodb.org/browse/SERVER-268 
-        # which prevent from bulk update of all records, uncomment the
-        # line below once it's fixed
-#        self.col.update(cond, {'$inc' : {'counter':1}})
-        # meanwhile we'll retrieve all records and update them individually
-        for record in self.col.find(cond):
-            self.col.update({'_id':record['_id']}, {'$inc' : {'counter':1}})
+        self.col.update(cond, {'$inc' : {'counter':1}})
 
     def list_systems(self):
         """
@@ -164,7 +147,7 @@ class DASAnalytics(object):
                 self.col.find(cond, ['api.params']))
         return gen2list(gen)
 
-    def api_counter(self, api, args={}):
+    def api_counter(self, api, args=None):
         """
         Retrieve API counter from analytics DB. User must supply
         API name and optional dict of parameters.
