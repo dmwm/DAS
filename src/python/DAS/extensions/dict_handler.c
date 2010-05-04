9581f172516edb667eb36d51d2f60ec44e374e25
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 23:50:28 +0000

    New implementation C-extension for dict_helper
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@2023 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/extensions/dict_handler.c
+++ src/python/DAS/extensions/dict_handler.c
@@ -1,31 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Python.h>
-/*
-#include <regex.h>
-
-static int rematch(const char *re, const char *s)
-{
-    int result;
-    regmatch_t match;
-    regex_t compiled;
-  
-    result = regcomp (&compiled, re, REG_EXTENDED | REG_ICASE | REG_NOSUB);
-    if (result) {
-        char errorstring[128];
-      
-        regerror (result, &compiled, errorstring, sizeof(errorstring));
-        regfree (&compiled);
-        printf("Error %d \n", result);
-        return result; // False
-    }
-  
-    result = regexec (&compiled, s, 1, &match, 0);
-    regfree (&compiled);
-    return result; // 0 is True
-}
-*/
-
 /* 
  * C-version of dict_helper from utils/utils.py
  * We need to perform mapping of parsed record into DAS notations
@@ -37,64 +12,33 @@ static int rematch(const char *re, const char *s)
 static PyObject*
 _dict_handler(PyObject *self, PyObject *args)
 {
-/*    char *pat_int = "(^[0-9]$|^[0-9][0-9]*$)";*/
-/*    char *pat_float = "(^[0-9]+.[0-9]*$|^[0-9]*.{1,1}[0-9]+$)";*/
     PyObject* dict;
     PyObject* map;
     PyObject* data = PyDict_New();
     if (!PyArg_ParseTuple(args, "OO", &dict, &map))
             return NULL;
 
-    PyObject *iter = NULL, *res = NULL, *key = NULL, *item = NULL, *val = NULL;
-    iter = PyObject_GetIter(dict);
-    if (iter == NULL)
-        return dict;
-
-    const char* cstr;
-    Py_ssize_t len;
-    while((item = PyIter_Next(iter))) {
-        key = PyDict_GetItem(map, item);
-        val = PyDict_GetItem(dict, item);
-
-        res = val;
-        if (val && PyString_Check(val) &&
-                PyObject_AsCharBuffer(val, &cstr, &len) == 0) {
-
-            const char* copy = cstr;
-            int test_float = 0;
-            while(*copy != NULL) {
-                if (*copy=='.') {
-                    test_float=1;
-                    break;
-                }
-                *copy++;
-            }
-            if (test_float==1) {
-                res = PyFloat_FromString(val, NULL);
-                if (res == NULL)
-                    res = val;
-            } else {
-                res = PyInt_FromString(cstr, NULL, 10);
-                if (res == NULL)
-                    res = val;
-            }
-
-        } else {
-/*            PyErr_SetString(PyExc_ValueError, "could not convert value to buffer");*/
-            PyErr_Print();
-            res = val;
+    PyObject *key = NULL, *val = NULL;
+    Py_ssize_t pos = 0;
+    while (PyDict_Next(dict, &pos, &key, &val)) {
+        PyObject *mkey = PyDict_GetItem(map, key);
+        PyObject *res = PyFloat_FromString(val, NULL);
+        int decr = 1;
+        if (res == NULL) {
+            res  = val;
+            decr = 0;
         }
-
-        if  (key!= NULL) {
+    
+        if  (mkey != NULL) { 
+            PyDict_SetItem(data, mkey, res); 
+            Py_XDECREF(mkey);
+        } else { 
             PyDict_SetItem(data, key, res);
-        } else {
-            PyDict_SetItem(data, item, res);
-        }
-        Py_XDECREF(item);
-    }
+        }   
+        if (decr == 1)
+            Py_XDECREF(res);
+    }       
     PyErr_Clear();
-
-    Py_XDECREF(iter);
     return data;
 }
 
