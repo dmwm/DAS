ff6c7a20e463bafd43287314568b8abdc085969b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 23:57:08 +0000

    Add DAS map validator and associative unit test for it
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@2175 4525493e-7705-40b1-a816-d608a930855b

--- test/map_reader_t.py
+++ test/map_reader_t.py
@@ -8,16 +8,16 @@ Unit test for DAS map reader
 import yaml
 import unittest
 import tempfile
-from DAS.services.map_reader import read_service_map
+from DAS.services.map_reader import read_service_map, validator
 
 class testDBS(unittest.TestCase):
     """
     A test class for the DAS map reader
     """
 
-    def testReader(self): 
-        """test read_service_map function"""
-        apimap  = {
+    def setUp(self):
+        """set up unit test data records"""
+        self.apimap  = {
             "url": "https://a.b.com", "system": "sitedb", 
             "urn": "CMSNametoSE",
             "format": "XML", "wild_card": "*",
@@ -26,7 +26,21 @@ class testDBS(unittest.TestCase):
             "apitag": None,
             "daskeys": [{"map": "site.name", "key": "site", "pattern": ""}],
             "das2api": [{"pattern": "", "das_key": "site", "api_param": "name"}], 
+            "created": 123
         }
+        self.presentation = {"presentation": 
+                {"city": [{"ui": "City", "das": "city.name"}, 
+                          {"ui": "Address", "das": "city.Placemark.address"}]},
+                "created":123}
+        self.notations = {"notations": 
+                [{"map": "name", "api": "", "notation": "cmsname"}, 
+                 {"map": "name", "api": "", "notation": "cms_name"}], 
+                "system": "sitedb", "created": 123}
+
+    def testReader(self): 
+        """test read_service_map function"""
+        apimap  = dict(self.apimap)
+        del apimap['created']
         fdescr  = tempfile.NamedTemporaryFile()
         mapfile = fdescr.name
         stream  = file(mapfile, 'w')
@@ -34,6 +48,30 @@ class testDBS(unittest.TestCase):
         result  = [r for r in read_service_map(mapfile)][0]
         result.pop('created')
         self.assertEqual(apimap, result)
+
+    def testValidator(self):
+        """test validator function"""
+        record = dict(self.apimap)
+        result = validator(record)
+        self.assertEqual(True, result)
+        del record['created']
+        result = validator(record)
+        self.assertEqual(False, result)
+
+        record = dict(self.presentation)
+        result = validator(record)
+        self.assertEqual(True, result)
+        del record['created']
+        result = validator(record)
+        self.assertEqual(False, result)
+
+        record = dict(self.notations)
+        result = validator(record)
+        self.assertEqual(True, result)
+        del record['created']
+        result = validator(record)
+        self.assertEqual(False, result)
+
 
 #
 # main
