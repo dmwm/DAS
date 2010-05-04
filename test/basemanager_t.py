c72ed517a8da8a5853af81f33cb6ad88b71b2043
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 21:58:38 +0000

    Major upgrade. Code refactoring to use cartesian product based on relation keys. Re-design data-services layer. Re-wrote sitedb/phedex/dbs layers to represent their data in unique set according to DAS architecture
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@149 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/basemanager_t.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#pylint: disable-msg=c0301,c0103
+
+"""
+unit test for base manager module
+"""
+
+import unittest
+from utils.das_config import das_readconfig
+from core.basemanager import BaseManager
+
+class testBaseManager(unittest.TestCase):
+    """
+    a test class for the das base manager module
+    """
+    def setUp(self):
+        """
+        set up das core module
+        """
+        dasconfig = das_readconfig()
+        self.mgr = BaseManager(dasconfig)
+
+    def test_result(self):                          
+        """test base manager result method"""
+        query  = "find site where site=T2_UK"
+        result = self.mgr.result(query)
+        expect = None
+        self.assertEqual(expect, result)
+#
+# main
+#
+if __name__ == '__main__':
+    unittest.main()
+
