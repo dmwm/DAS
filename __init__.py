793f9e640f3a1ab1c4bde424b26bf99cf6a5553e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 21:58:02 +0000

    init
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@132 4525493e-7705-40b1-a816-d608a930855b

--- __init__.py
+++ __init__.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 #-*- coding: ISO-8859-1 -*-
 
-"""
-DAS phedex service module
-"""
-
-__revision__ = "$Id: __init__.py,v 1.1 2009/03/09 19:43:33 valya Exp $"
+__revision__ = "$Id: __init__.py,v 1.1 2009/01/23 21:47:41 valya Exp $"
 __version__ = "$Revision: 1.1 $"
 __author__ = "Valentin Kuznetsov"
--- bin/das_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-find $DAS_ROOT/test -name "*_t.py" | \
-awk '{print "echo; echo \"running unit tests for "$0"\"; python "$0""}' | \
-/bin/sh
