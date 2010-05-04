0eedc1419151841113942a8fcdd6944f08ed6a3f
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:02:24 +0000

    Finish work with QL parser. Enable it. Adjust services and unit tests
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@243 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/services/sitedb/sitedb_service.py
+++ src/python/DAS/services/sitedb/sitedb_service.py
@@ -4,14 +4,13 @@
 """
 SiteDB service
 """
-__revision__ = "$Id: sitedb_service.py,v 1.5 2009/05/11 20:21:20 valya Exp $"
-__version__ = "$Revision: 1.5 $"
+__revision__ = "$Id: sitedb_service.py,v 1.6 2009/05/13 15:19:33 valya Exp $"
+__version__ = "$Revision: 1.6 $"
 __author__ = "Valentin Kuznetsov"
 
 import re
 import types
 from DAS.services.abstract_service import DASAbstractService
-from DAS.utils.utils import query_params, cartesian_product
 from DAS.utils.utils import add2dict, map_validator
 
 class SiteDBService(DASAbstractService):
@@ -56,6 +55,31 @@ class SiteDBService(DASAbstractService):
         }
         map_validator(self.map)
 
+    def adjust(self, apidict):
+        """
+        Fix apidict if API requires CMS name and provided name is se one.
+        And to reverse if necessary.
+        """
+        pat = re.compile('^CMSName')
+        for api, params in apidict.items():
+            if  pat.match(api) and params.has_key('name'):
+                nlist = []
+                for val in params['name']:
+                    if  val.find('.') != -1:
+                        newval = self.se2cms(val)
+                        nlist.append(newval)
+                params['name'] = nlist
+
+    def adjust_result(self, api, idict):
+        """
+        Convert sitedb dict into expected dict structure
+        {'key':[results]}.
+        """
+        jsondict = {}
+        for key in self.map[api]['keys'] :
+            jsondict[key] = idict.values()
+        return jsondict
+
     def se2cms(self, site_se):
         """
         Convert SE to CMS names.
@@ -96,6 +120,7 @@ class SiteDBService(DASAbstractService):
                 break
         return api, args
 
+
     def site2se(self, site):
         """
         convert given site name to SE
@@ -123,61 +148,6 @@ class SiteDBService(DASAbstractService):
             sitese = site
         return sitese
 
-    def api(self, query, cond_dict=None):
-        """
-        A service worker. It parses input query, invoke service API 
-        and return results in a list with provided row.
-        """
-        selkeys, cond = query_params(query)
-        params = {}
-        for key in cond.keys():
-            oper, val = cond[key]
-            if  oper == '=':
-                params[key] = val
-            else:
-                raise Exception("DAS::%s, not supported operator '%s'" \
-                % (self.name, oper))
-        for key in cond_dict:
-            if  not params.has_key(key):
-                params[key] = cond_dict[key]
-
-        if  params.has_key('site'):
-            sitename = params['site']
-        else:
-            sitename = ''
-        if  type(sitename) is types.ListType:
-            sitelist = list(sitename)
-        else:
-            if  not sitename:
-                sitelist = self.cms2se()
-            else:
-                sitelist = [sitename]
-
-        data = []
-        resdict = {}
-        # HACK to use 1 arg and 1 sel keys
-        if  len(sitelist) == 1 and len(selkeys) == 1:
-            api, args = self.findapi(selkeys[0], sitelist[0])
-            jsondict  = self.call_service_api(api, args)
-            data = []
-            for key, val in jsondict.items():
-                row = {selkeys[0]: val['name']}
-                data.append(row)
-            return data
-        # TODO: review how to abstract SiteDB
-        for site in sitelist:
-            for key in selkeys:
-#                if  key == 'site':
-#                    continue
-                api, args = self.findapi(key, site)
-                jsondict = self.call_service_api(api, args)
-                data = getattr(self, 'parser_%s' % api)(jsondict, site)
-                add2dict(resdict, api, data)
-        # create cartesian product of out results
-        rel_keys = ['site']
-        data = self.product(resdict, rel_keys)
-        return data
-
     def call_service_api(self, apiname, params):
         """
         Call SiteDB API, since its return type is JSON, we eval the results to
@@ -186,85 +156,3 @@ class SiteDBService(DASAbstractService):
         url = self.url + '/' + apiname
         data  = eval(self.getdata(url, params))
         return data
-
-    def parser_CMSNametoAdmins(self, jsondict, site):
-        """
-        Parse data from CMSNametoAdmins SiteDB API
-        """
-        newrow  = {}
-        data    = []
-        sites   = self.site2se(site)
-        for val in jsondict.values():
-            row = dict(newrow)
-            res = '%s %s (%s)' % \
-            (val['forename'], val['surname'], val['email'])
-            row['admin'] = res
-            if  type(sites) is types.StringType:
-                row['site'] = sites
-                if  not data.count(row):
-                    data.append(row)
-            else:
-                for se in sites:
-                    serow = dict(row)
-                    serow['site'] = se
-                    if  not data.count(serow):
-                        data.append(serow)
-        return data
-    
-    def parser_common(self, key, jsondict, site, name='name'):
-        """
-        Common parser for data from SiteDB API
-        """
-        newrow = {}
-        data = []
-        for val in jsondict.values():
-            row = dict(newrow)
-            res = val[name]
-            row[key] = res
-            row['site'] = self.site2se(site)
-            data.append(row)
-        return data
-
-    def parser_SEtoCMSName(self, jsondict, site):
-        """
-        Parse data from SEtoCMSName SiteDB API
-        """
-        return self.parser_common('site.cmsname', jsondict, site)
-
-    def parser_CMStoSAMName(self, jsondict, site):
-        """
-        Parse data from CMStoSAMName SiteDB API
-        """
-        return self.parser_common('site.samname', jsondict, site)
-
-    def parser_CMStoSiteName(self, jsondict, site):
-        """
-        Parse data from CMStoSiteName SiteDB API
-        """
-        return self.parser_common('site.sitename', jsondict, site)
-
-    def parser_CMSNametoCE(self, jsondict, site):
-        """
-        Parse data from CMSNametoCE SiteDB API
-        """
-        return self.parser_common('site.cename', jsondict, site)
-
-    def parser_CMSNametoSE(self, jsondict, site):
-        """
-        Parse data from CMSNametoSE SiteDB API
-        """
-        return self.parser_common('site.sename', jsondict, site)
-
-    def parser_CMSNametoPhEDExNode(self, jsondict, site):
-        """
-        Parse data from CMSNametoPhEDExNode SiteDB API
-        """
-        return self.parser_common('site.phedexname', jsondict, site, 
-                                  'phedex_node')
-
-    def parser_SiteStatus(self, jsondict, site):
-        """
-        Parse data from SiteStatus SiteDB API
-        """
-        return self.parser_common('site.status', jsondict, site, 'status')
-
--- test/das_mapping_t.py
+++ test/das_mapping_t.py
@@ -38,6 +38,39 @@ class testDASMapping(unittest.TestCase):
         expect = ['T2', ['T2', 'T3']]
         self.assertEqual(expect, result)
 
+    def test_jsonparser_sitedb(self): 
+        """test DAS jsonparser routine with site db data"""
+        jsondict = {'0': {'name': 'cmsrm-se01.roma1.infn.it'}}
+        keylist  = ['admin', 'name']
+        result   = jsonparser('sitedb', jsondict, keylist)
+        # NOTE: sitedb uses site for many purposes, in this case
+        # the name maps to site.cename, site.sename, site.cmsname, site
+        expect   = [{'admin':'', 'site.cename': 'cmsrm-se01.roma1.infn.it', 
+                     'site.sename': 'cmsrm-se01.roma1.infn.it', 
+                     'site.cmsname': 'cmsrm-se01.roma1.infn.it', 
+                     'site': 'cmsrm-se01.roma1.infn.it'}]
+        self.assertEqual(expect, result)
+
+    def test_jsonparser_sitedb_admin(self): 
+        """test DAS jsonparser routine with site db data for admins"""
+        jsondict = {'admin': [{'title': 'Data Manager', 'surname': 'Rahatlou', 
+                          'email': 'Shahram.Rahatlou@cern.ch', 'forename': 'Shahram'}, 
+                    {'title': 'Data Manager', 'surname': 'M.Barone (Red Baron)', 
+                          'email': 'luciano.barone@cern.ch', 'forename': 'Luciano'}]}
+        keylist  = ['admin', 'name']
+        result   = jsonparser('sitedb', jsondict, keylist)
+        expect   = [{'admin': {'forename': 'Shahram', 'surname': 'Rahatlou', 
+                               'email': 'Shahram.Rahatlou@cern.ch', 
+                               'title': 'Data Manager'}, 
+                     'site.cename': '', 'site.sename': '', 'site': '', 
+                     'site.cmsname': ''}, 
+                    {'admin': {'forename': 'Luciano', 'surname': 'M.Barone (Red Baron)', 
+                               'email': 'luciano.barone@cern.ch', 
+                               'title': 'Data Manager'}, 
+                     'site.cename': '', 'site.sename': '', 'site': '', 
+                     'site.cmsname': ''}]
+        self.assertEqual(expect, result)
+
     def test_jsonparser(self): 
         """test DAS jsonparser routine"""
         rep1 = {'bytes':1, 'files':1, 'se':'T2'}
@@ -48,8 +81,6 @@ class testDASMapping(unittest.TestCase):
         result = jsonparser('phedex', jsondict, ['block.name','block.bytes'])
         expect = [{'block': 'ABC', 'block.size': 1}, 
                   {'block': 'CDE', 'block.size': 2}]
-#        expect = [{'block.name': 'ABC', 'block.bytes': 1}, 
-#                  {'block.name': 'CDE', 'block.bytes': 2}]
         self.assertEqual(expect, result)
 
         result = jsonparser('phedex', jsondict, ['block.name','block.replica.se'])
@@ -57,10 +88,6 @@ class testDASMapping(unittest.TestCase):
                   {'block': 'CDE', 'site': 'T2'},
                   {'block': 'CDE', 'site': 'T3'},
                  ]
-#        expect = [{'block.name': 'ABC', 'block.replica.se': 'T2'}, 
-#                  {'block.name': 'CDE', 'block.replica.se': 'T2'},
-#                  {'block.name': 'CDE', 'block.replica.se': 'T3'},
-#                 ]
         self.assertEqual(expect, result)
 
 #        json={'phedex': {'request_date': '2009-04-15 19:18:02 UTC', 'request_timestamp': 1239823082.5613501, 'call_time': '0.00937', 'instance': 'prod', 'request_call': 'blockReplicas', 'request_url': 'http://cmsweb.cern.ch:7001/phedex/datasvc/json/prod/blockReplicas', 'request_version': '1.3.1', 'block': [{'files': '4', 'name': '/Cosmics/CRUZET4_v1_CRZT210_V1_SuperPointing_v1/RECO#96c1b23f-1d88-4aa5-96ed-966a73a38c2d', 'bytes': '291825111', 'replica': [{'files': '4', 'node': 'T1_US_FNAL_Buffer', 'group': 'null', 'complete': 'y', 'time_create': '1219412876.0661', 'custodial': 'n', 'bytes': '291825111', 'time_update': '1229179156.51997', 'node_id': '9', 'se': 'cmssrm.fnal.gov'}, {'files': '4', 'node': 'T1_US_FNAL_MSS', 'group': 'null', 'complete': 'y', 'time_create': '1219413379.70068', 'custodial': 'y', 'bytes': '291825111', 'time_update': '1229179156.51997', 'node_id': '10', 'se': 'cmssrm.fnal.gov'}, {'files': '4', 'node': 'T2_CH_CAF', 'group': 'null', 'complete': 'y', 'time_create': '1219413630.40003', 'custodial': 'n', 'bytes': '291825111', 'time_update': '1229179156.51997', 'node_id': '501', 'se': 'caf.cern.ch'}], 'is_open': 'n', 'id': '426474'}]}}
