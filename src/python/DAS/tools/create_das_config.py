613d6aa7037ecbce82df53b90b140dc4e6301412
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:00:58 +0000

    Adjust code to pass pylint quality requirements
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@207 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/tools/create_das_config.py
+++ src/python/DAS/tools/create_das_config.py
@@ -4,18 +4,16 @@
 """
 DAS config generator
 """
-__revision__ = "$Id: create_das_config.py,v 1.2 2009/04/07 19:54:17 valya Exp $"
-__version__ = "$Revision: 1.2 $"
+__revision__ = "$Id: create_das_config.py,v 1.3 2009/04/29 19:52:12 valya Exp $"
+__version__ = "$Revision: 1.3 $"
 __author__ = "Valentin Kuznetsov"
 
-import os
 from DAS.utils.das_config import das_writeconfig
 
 #
 # main
 #
 if __name__ == '__main__':
-    file = das_writeconfig()
-#    file = os.path.join(os.getcwd(), 'das.cfg')
+    dasconfig = das_writeconfig()
     print "DAS configuration file has been created"
-    print file
+    print dasconfig
