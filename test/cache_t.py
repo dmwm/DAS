d283c6dbbcfcba9742dd93859bedb5797f5403e3
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 23:17:26 +0000

    unit test for base cache class
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@1241 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/cache_t.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#pylint: disable-msg=c0301,c0103
+
+"""
+unit test for cache module
+"""
+
+import unittest
+from core.cache import Cache
+from core.das_core import DASCore
+
+class testCache(unittest.TestCase):
+    """
+    a test class for the das cache module
+    """
+    def setUp(self):
+        """
+        set up das core module
+        """
+        self.dascache = Cache(debug=1)
+        self.das = DASCore()
+
+    def test_result(self):                          
+        """test cache result method"""
+        query  = "find site where site=T2_UK"
+        result = self.dascache.result(query)
+        result.sort()
+        expect = self.das.result(query)
+        expect.sort()
+        self.assertEqual(expect, result)
+#
+# main
+#
+if __name__ == '__main__':
+    unittest.main()
+
