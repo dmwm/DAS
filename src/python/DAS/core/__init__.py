b7f54bc04cdd21067594313a2c2893b5bb2ce71b
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:24:30 +0000

    Adjust docstrings to sphinx syntax
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@785 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/core/__init__.py
+++ src/python/DAS/core/__init__.py
@@ -2,9 +2,11 @@
 #-*- coding: ISO-8859-1 -*-
 
 """
-DAS core module
+DAS core modules
+
+.. moduleauthor:: Valentin Kuznetsov <vkuznet@gmail.com>
 """
 
-__revision__ = "$Id: __init__.py,v 1.1 2009/03/13 21:10:03 valya Exp $"
-__version__ = "$Revision: 1.1 $"
+__revision__ = "$Id: __init__.py,v 1.2 2010/01/25 20:18:16 valya Exp $"
+__version__ = "$Revision: 1.2 $"
 __author__ = "Valentin Kuznetsov"
