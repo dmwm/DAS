a3fd9213977835863523dfa02f734c183b38ce38
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:38:05 +0000

    Adjust code to pass 8/10 of code quality using pyling
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@1103 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/analytics/analytics_service.py
+++ src/python/DAS/analytics/analytics_service.py
@@ -9,37 +9,43 @@ class DASServiceBase(multiprocessing.Process):
     defaults = {'interval':1}
     
     @classmethod
-    def merge_defaults(cls,config):
+    def merge_defaults(cls, config):
+        """merge class defaults"""
         default_copy = copy.deepcopy(cls.defaults)
         default_copy.update(config)
         return default_copy
     
     @staticmethod
     def validate_config(config):
+        """validate configuration"""
         return True
     
-    def __init__(self,name,config,controller_pipe):
+    def __init__(self, name, config, controller_pipe):
         multiprocessing.Process.__init__(self)
         self.config = config
         self._pipe = controller_pipe
         self.name = name
-        self.log(0,"created")
+        self.log(0, "created")
         self._ticks = 0
         self._time_started = 0
         self._stop = False
         self.configure(config)
     
-    def configure(self,config):
+    def configure(self, config):
+        """configure the service"""
         pass
         
     def service_start(self):
+        """start the service"""
         pass
     
     def service_run(self):
+        """run the service"""
         pass
     
     def run(self):
-        self.log(0,"started")
+        """run the service"""
+        self.log(0, "started")
         self._time_started = time.time()
         self.service_start()
         while True:
@@ -50,20 +56,23 @@ class DASServiceBase(multiprocessing.Process):
             self.service_run()
             time.sleep(self.config['interval'])
             
-    def log(self,importance,message):
-        self._pipe.send({'type':'log','time':time.time(),'level':importance,'text':message, 'src':self.name})
+    def log(self, importance, message):
+        """Logger method"""
+        self._pipe.send({'type':'log', 'time':time.time(),
+                         'level':importance, 'text':message, 'src':self.name})
     
     def _pipe_handler(self):
+        """Pipe handler"""
         while self._pipe.poll():
             msg = self._pipe.recv()
-            if msg and isinstance(msg,dict):
-                msg['type'] = msg.get('type','default')
-                funcname = 'pipe_%s'%msg['type']
-                if hasattr(self,funcname):
-                    if callable(getattr(self,funcname)):
-                        retval = getattr(self,funcname)(msg)
-                        if not retval==None:
-                            if not isinstance(retval,dict):
+            if msg and isinstance(msg, dict):
+                msg['type'] = msg.get('type', 'default')
+                funcname = 'pipe_%s' % msg['type']
+                if hasattr(self, funcname):
+                    if callable(getattr(self, funcname)):
+                        retval = getattr(self, funcname)(msg)
+                        if not retval == None:
+                            if not isinstance(retval, dict):
                                 retval = {'data':retval}
                             retval['src'] = self.name
                             retval['type'] = msg['type']
@@ -73,29 +82,37 @@ class DASServiceBase(multiprocessing.Process):
                 else:
                     self.pipe_default(msg)
             else:
-                self.log(1,'Invalid pipe msg: %s'%msg)
+                self.log(1, 'Invalid pipe msg: %s' % msg)
     
-    def pipe_default(self,msg):
-        self.log(1,'Attempted to call invalid pipe function "%s"'%msg['type'])
+    def pipe_default(self, msg):
+        """Default method for pipe"""
+        self.log(1, 'Attempted to call invalid pipe function "%s"' % msg['type'])
                 
-    def pipe_ping(self,msg):
+    def pipe_ping(self, msg):
+        """ping the pipe"""
         return msg
     
-    def pipe_stats(self,msg):
+    def pipe_stats(self, msg):
+        """pipe stats"""
         return {}
         
-    def pipe_statdesc(self,msg):
+    def pipe_statdesc(self, msg):
+        """pipe status description"""
         return {}
     
-    def pipe_stop(self,msg):
+    def pipe_stop(self, msg):
+        """stop the pipe"""
         self._stop = True
         self.finalise()
         
-    def pipe_started(self,msg):
+    def pipe_started(self, msg):
+        """start the pipe"""
         return self._time_started
     
-    def pipe_status(self,msg):
+    def pipe_status(self, msg):
+        """Pipe status"""
         return "Still Alive"
     
     def finalise(self):
+        """Finalise"""
         pass
--- src/python/DAS/analytics/services/logger.py
+++ src/python/DAS/analytics/services/logger.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+
+"""
+Logger module
+"""
+
+__revision__ = "$Id: logger.py,v 1.2 2010/04/06 20:34:39 valya Exp $"
+__version__ = "$Revision: 1.2 $"
+__author__ = "Gordon Ball"
+
 import DAS.analytics.analytics_service
-import sys
 import time
 
 class Logger(DAS.analytics.analytics_service.DASServiceBase):
+    """Logger class"""
     defaults = {
                 'interval':1,
                 'output':'/tmp/das_analytics_log',
-                'subscriptions':[('all','*','log')],
+                'subscriptions':[('all', '*', 'log')],
                 'flush_frequency':5
                 }
     levels = {
@@ -14,25 +25,28 @@ class Logger(DAS.analytics.analytics_service.DASServiceBase):
               1: 'ERROR',
               2: 'CRITICAL'
               }
-    def configure(self,config):
-        self.output = open(config['output'],'w')
+    def configure(self, config):
+        """Configure the logger"""
+        self.output = open(config['output'], 'w')
         self.flush_frequency = config['flush_frequency']
         self.flush_count = 0
         
-        #self.output = sys.stdout
-    
-    def pipe_log(self,msg):
+    def pipe_log(self, msg):
+        """Pipe log"""
         print 'pipe_log'
-        src = msg.get('src','')
-        level = msg.get('level',0)
-        t = msg.get('time',0)
-        text = msg.get('text','')
-        self.output.write('[%s] %s: %s: %s\n'%(time.strftime('%d-%m-%Y %H:%M:%S',time.gmtime(t)),src,self.levels.get(level,'OTHER'),text))
+        src = msg.get('src', '')
+        level = msg.get('level', 0)
+        tstamp = msg.get('time', 0)
+        text = msg.get('text', '')
+        self.output.write('[%s] %s: %s: %s\n' % \
+                (time.strftime('%d-%m-%Y %H:%M:%S', time.gmtime(tstamp)),
+                        src, self.levels.get(level, 'OTHER'), text))
         self.flush_count += 1
-        if self.flush_count%self.flush_frequency:
+        if self.flush_count % self.flush_frequency:
             self.output.flush()
         
     def finalise(self):
+        """Close the output"""
         self.output.close()
         
     
