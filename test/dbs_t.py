46516a61c977c94ec9695f9b6945fc71aba16a2c
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 21:58:05 +0000

    initial commit of DAS code
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@133 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/dbs_t.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#pylint: disable-msg=C0301,C0103
+
+"""
+Unit test for DAS DBS module
+"""
+
+import unittest
+from services.dbs.dbs_parser import parser
+
+class testDBS(unittest.TestCase):
+    """
+    A test class for the DAS DBS module
+    """
+
+    def testParser(self):                          
+        """test call routine"""
+        data = """<?xml version='1.0' standalone='yes'?>
+<result STORAGEELEMENT_SENAME='se' FILES_LOGICALFILENAME='test'/>
+"""
+        resultlist = parser(data)
+        expectlist = [{'STORAGEELEMENT_SENAME': 'se', 
+                       'FILES_LOGICALFILENAME': 'test'}]
+        self.assertEqual(expectlist, resultlist)
+
+#
+# main
+#
+if __name__ == '__main__':
+    unittest.main()
+
+
