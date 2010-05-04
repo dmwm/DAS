ae1049389dae69463fdd62ce8071b827d47e367e
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:09:41 +0000

    Change update method to accept the system name, change order of arguments in add_api, added index on system,query,api.name.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@424 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/core/das_analytics_db.py
+++ src/python/DAS/core/das_analytics_db.py
@@ -7,8 +7,8 @@ DAS analytics DB
 
 from __future__ import with_statement
 
-__revision__ = "$Id: das_analytics_db.py,v 1.4 2009/09/11 15:59:57 valya Exp $"
-__version__ = "$Revision: 1.4 $"
+__revision__ = "$Id: das_analytics_db.py,v 1.5 2009/09/11 18:42:14 valya Exp $"
+__version__ = "$Revision: 1.5 $"
 __author__ = "Valentin Kuznetsov"
 
 import os
@@ -16,6 +16,7 @@ import traceback
 
 # monogo db modules
 from pymongo.connection import Connection
+from pymongo import DESCENDING
 
 # DAS modules
 from DAS.utils.utils import getarg, gen2list
@@ -53,28 +54,34 @@ class DASAnalytics(object):
         """
         self.conn.drop_database(self.dbname)
 
-    def add_api(self, query, system, api, args):
+    def add_api(self, system, query, api, args):
         """
         Add API info to analytics DB. 
         Here args is a dict of API parameters.
         """
         msg = 'DASAnalytics::add_api(%s, %s, %s, %s)' \
-        % (query, system, api, args)
+        % (system, query, api, args)
         self.logger.info(msg)
         apidict = dict(name=api, params=args)
         record = dict(query=query, system=system, api=apidict, counter=1)
         if  self.col.find({'query':query, 'system':system, 'api.name':api}).count():
             return self.update(query)
         self.col.insert(record)
+        index = [('system', DESCENDING), ('query', DESCENDING),
+                 ('api.name', DESCENDING) ]
+        self.col.ensure_index(index)
 
-    def update(self, query):
+    def update(self, system, query):
         """
         Update record with given query.
         """
-        cond = {'query':query}
+        msg = 'DASAnalytics::update(%s, %s)' % (system, query)
+        self.logger.info(msg)
+        cond = {'query':query, 'system':system}
         for row in self.col.find(cond):
+            id = row['_id']
             row['counter'] += 1
-            self.col.update(cond, row)
+            self.col.update({'_id':id}, row)
 
     def list_systems(self):
         """
