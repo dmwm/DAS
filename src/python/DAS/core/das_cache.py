cf73c85d53bdc1804abb0ee40dd56e4659af09e4
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 22:24:10 +0000

    Remove obsolete code
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@777 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/core/das_cache.py
+++ src/python/DAS/core/das_cache.py
@@ -5,12 +5,11 @@
 DAS cache wrapper. Communitate with DAS core and cache server(s)
 """
 
-__revision__ = "$Id: das_cache.py,v 1.20 2010/01/15 17:17:11 valya Exp $"
-__version__ = "$Revision: 1.20 $"
+__revision__ = "$Id: das_cache.py,v 1.21 2010/01/19 19:05:26 valya Exp $"
+__version__ = "$Revision: 1.21 $"
 __author__ = "Valentin Kuznetsov"
 
 import time
-import Queue 
 import traceback
 try:
     # Python 2.6
@@ -24,96 +23,6 @@ import logging
 
 # DAS modules
 from DAS.utils.utils import getarg, genkey
-from DAS.core.cache import Cache, NoResults
-from DAS.core.das_memcache import DASMemcache
-from DAS.core.das_couchcache import DASCouchcache
-from DAS.core.das_filecache import DASFilecache
-
-class DASCache(Cache):
-    """
-    Base DAS cache class based on memcache and couchdb cache back-end
-    servers. The class should be initialized with config dict.
-    """
-    def __init__(self, config):
-        Cache.__init__(self, config)
-        self.servers = {'memcache': DASMemcache(config),
-                        'filecache': DASFilecache(config),
-                        'couchcache': DASCouchcache(config),
-        }
-        self.logger.info('DASCache using servers = %s' % self.servers)
-
-    def incache(self, query):
-        """
-        Retreieve results from cache, otherwise return null.
-        """
-        servers = self.servers.keys()
-        servers.sort()
-        for name in servers:
-            self.logger.info("DASCache::incache, using %s" % name)
-            srv = self.servers[name]
-            res = srv.incache(query)
-            if  res:
-                return res
-
-    def get_from_cache(self, query, idx=0, limit=0):
-        """
-        Retreieve results from cache, otherwise return null.
-        """
-        servers = self.servers.keys()
-        servers.sort()
-        for name in servers:
-            self.logger.info("DASCache::get_from_cache, using %s" % name)
-            srv = self.servers[name]
-            res = srv.get_from_cache(query, idx, limit)
-            if  res:
-                return res
-
-    def update_cache(self, query, results, expire):
-        """
-        Insert results into cache.
-        """
-        servers = self.servers.keys()
-        servers.sort()
-        for name in servers:
-            self.logger.info("DASCache::update_cache, using %s" % name)
-            srv = self.servers[name]
-            results = srv.update_cache(query, results, expire)
-        for item in results:
-            yield item
-
-    def clean_cache(self, cache=None):
-        """
-        Clean expired results in cache.
-        """
-        if  cache: # request to update particular cache server
-            if  not self.servers.has_key(cache):
-                raise Exception("DASCache doesn't support cache='%s'" % cache)
-            srv = self.servers[cache]
-            srv.clean_cache()
-            return
-        servers = self.servers.keys()
-        servers.sort()
-        for name in servers:
-            self.logger.info("DASCache::clean_cache, using %s" % name)
-            srv = self.servers[name]
-            srv.clean_cache()
-
-    def delete_cache(self, dbname='das', cache=None):
-        """
-        Delete all results in cache.
-        """
-        if  cache: # request to update particular cache server
-            if  not self.servers.has_key(cache):
-                raise Exception("DASCache doesn't support cache='%s'" % cache)
-            srv = self.servers[cache]
-            srv.delete_cache(dbname)
-            return
-        servers = self.servers.keys()
-        servers.sort()
-        for name in servers:
-            self.logger.info("DASCache::clean_cache, using %s" % name)
-            srv = self.servers[name]
-            srv.delete_cache(dbname)
 
 class DASCacheMgr(object):
     """
