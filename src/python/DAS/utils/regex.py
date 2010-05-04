e937bb5c99abdb04b13c79e62540f5105101e332
metson <metson@4525493e-7705-40b1-a816-d608a930855b>
2010-05-04 23:56:39 +0000

    Put all regular expression used in DAS code into one single place; provide unit tests to test each of them
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@2164 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/DAS/utils/regex.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+
+"""
+Regular expression patterns
+"""
+
+__revision__ = "$Id: regex.py,v 1.1 2010/03/19 17:22:45 valya Exp $"
+__version__ = "$Revision: 1.1 $"
+__author__ = "Valentin Kuznetsov"
+
+# system modules
+import re
+
+ip_address_pattern = \
+    re.compile(r"^([0-9]{1,3}\.){3,3}[0-9]{1,3}$")
+last_time_pattern = \
+    re.compile('^[0-9][0-9](h|m)$') # 24h or 12m
+date_yyyymmdd_pattern = \
+    re.compile('[0-2]0[0-9][0-9][0-1][0-9][0-3][0-9]')
+key_attrib_pattern = \
+    re.compile(r"^([a-zA-Z_]+\.?)+$") # match key.attrib
+cms_tier_pattern = \
+    re.compile('T[0-9]_') # T1_CH_CERN
+float_number_pattern = \
+    re.compile(r'(^[-]?\d+\.\d*$|^\d*\.{1,1}\d+$)')
+int_number_pattern = \
+    re.compile(r'(^[0-9-]$|^[0-9-][0-9]*$)')
+phedex_tier_pattern = \
+    re.compile('^T[0-9]_[A-Z]+(_)[A-Z]+') # T2_UK_NO
+se_pattern = \
+    re.compile('[a-z]+(\.)[a-z]+(\.)') # a.b.c
+site_pattern = \
+    re.compile('^[A-Z]+') 
+web_arg_pattern = \
+    re.compile('^[+]?\d*$') # used in web form, e.g idx=0
+number_pattern = \
+    re.compile('^[-]?[0-9][0-9\.]*$') # -123
