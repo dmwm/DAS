92945617bac81eb9265caa6ce4efc936e5d84635
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2010-05-10 16:12:07 +0000

    Added -# as valid characters in DAS values, e.g. block name
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@2376 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/core/das_lexer.py
+++ src/python/DAS/core/das_lexer.py
@@ -52,7 +52,7 @@ class DASLexer(object):
             return t
 
     def t_WORD(self, t):
-        r'[a-zA-Z/*][a-zA-Z_0-9/*]+|[0-9]+[dhm]|([0-9]{1,3}\.){3,3}[0-9]{1,3}'
+        r'[a-zA-Z/*][a-zA-Z_0-9/*\-#]+|[0-9]+[dhm]|([0-9]{1,3}\.){3,3}[0-9]{1,3}'
         cond1 = t.value not in self.daskeys
         list1 = t.value.split()
         cond2 = set(list1) & set(self.daskeys)
