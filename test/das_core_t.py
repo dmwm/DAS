2810563e09f980f64756c32e42fc3b2dd70f284d
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2010-05-10 18:49:09 +0000

    Added to the test _id for aggregated result output
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@2381 4525493e-7705-40b1-a816-d608a930855b

--- test/das_core_t.py
+++ test/das_core_t.py
@@ -54,7 +54,7 @@ class testDASCore(unittest.TestCase):
         result = self.das.call(query)
         result = self.das.get_from_cache(query)
         result = [r for r in result][0]
-        expect = {"function": "count", "result": 5, "key": "zip.Placemark.address"}
+        expect = {"function": "count", "result": 5, "key": "zip.Placemark.address", "_id":0}
         self.assertEqual(expect, result)
 
     def testIPService(self):
