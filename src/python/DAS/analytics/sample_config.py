9ac2291bbd86b067489fe9f1ff5bf2bc7356edd6
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2010-09-02 23:52:10 +0000

    Remove old analytics.
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@8803 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/analytics/sample_config.py
+++
@@ -1,5 +0,0 @@
-
-modules = ['services.logger']
-#analyser(classname,analysername,kwarg0=0,kwarg1=1)
-service('Logger')
-service('Logger', 'CoreLogger', output='/tmp/das_analytics_core_log', subscriptions=[('all', 'core', 'log')])
