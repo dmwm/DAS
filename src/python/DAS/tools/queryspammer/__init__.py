e20f36e014fbd962040f023ac8d0ce93c0797832
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2010-09-20 00:43:06 +0000

    Missing init
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@10320 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/DAS/tools/queryspammer/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+#pylint: disable-msg=C0103
+
+"""
+DAS query spammer toolkit 
+
+.. moduleauthor:: Gordon Ball <gordon.ball@cern.ch>
+"""
+
+__revision__ = "$Id: __init__.py,v 1.8 2010/04/30 17:14:35 valya Exp $"
+__version__ = "$Revision: 1.8 $"
+__author__ = "Gordon Ball"
+
