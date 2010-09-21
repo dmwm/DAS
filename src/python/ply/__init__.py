736f453a2745531513b954a44eb124ff8c8f3a84
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2010-09-21 16:03:19 +0000

    Add missing ply area (found via deployment test)
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@10356 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/ply/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+#pylint: disable-msg=C0103
+
+"""
+DAS PLY stuff
+
+.. moduleauthor:: Valentin Kuznetsov <vkuznet@gmail.com>
+"""
+
+__revision__ = "$Id: __init__.py,v 1.8 2010/04/30 17:14:35 valya Exp $"
+__version__ = "$Revision: 1.8 $"
+__author__ = "Valentin Kuznetsov"
