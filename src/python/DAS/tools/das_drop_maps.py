2fa939085548008ff45f433bda72dc22c030633e
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2010-12-06 17:45:34 +0000

    Add new tools to drop DAS YAML map files into JSON
    Signed-off-by: Valentin Kuznetsov <vkuznet@gmail.com>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@11052 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/DAS/tools/das_drop_maps.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+"""
+DAS map reader which transform DAS YML files into JSON docs and
+dump them on stdout
+"""
+__author__ = "Valentin Kuznetsov"
+
+import sys
+import json
+from optparse import OptionParser
+from DAS.services.map_reader import read_service_map
+
+class DASOptionParser: 
+    """
+    DAS cli option parser
+    """
+    def __init__(self):
+        self.parser = OptionParser()
+        self.parser.add_option("--uri-map", action="store", type="string",
+             default=None , dest="umap", help="specify uri map file")
+        self.parser.add_option("--notation-map", action="store", type="string",
+             default=None , dest="nmap", help="specify notation map file")
+        self.parser.add_option("--presentation-map", action="store", type="string",
+             default=None , dest="pmap", help="specify presentation map file")
+    def get_opt(self):
+        """
+        Returns parse list of options
+        """
+        return self.parser.parse_args()
+
+def nooutput(results):
+    """Just iterate over generator, but don't print it out"""
+    for row in results:
+        pass
+#
+# main
+#
+if __name__ == '__main__':
+    optManager  = DASOptionParser()
+    (opts, args) = optManager.get_opt()
+
+    if  opts.umap:
+        for rec in read_service_map(opts.umap, field='uri'):
+            print json.dumps(rec)
+
+    if  opts.nmap:
+        for rec in read_service_map(opts.nmap, field='notations'):
+            print json.dumps(rec)
+
+    if  opts.pmap:
+        for rec in read_service_map(opts.pmap, field='presentation'):
+            print json.dumps(rec)
+
