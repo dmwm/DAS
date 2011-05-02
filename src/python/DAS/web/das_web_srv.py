ad22c6de21c4a9a59ade568e6bd7cdab78cc8172
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2011-05-02 23:20:53 +0000

    Fix typo
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@12499 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/web/das_web_srv.py
+++ src/python/DAS/web/das_web_srv.py
@@ -143,7 +143,7 @@ def adjust_values(func, gen, links):
                     value = '<span %s>%s</span>' % (green, value)
                 else:
                     value = '<span %s>%s</span>' % (red, value)
-            if  key == 'File present':
+            if  key == 'File presence':
                 if  not value:
                     continue
                 else:
