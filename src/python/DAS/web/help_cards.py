d8bb4ad182eb6137896f648a7ae03d31651017a2
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2011-05-10 16:09:38 +0000

    Adjust help cards; fix offset in card div
    Signed-off-by: Valentin Kuznetsov <vkuznet@gmail.com>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@12596 4525493e-7705-40b1-a816-d608a930855b

--- src/python/DAS/web/help_cards.py
+++ src/python/DAS/web/help_cards.py
@@ -24,6 +24,7 @@ DAS queries are formed by
 <li>run=148126</li>
 </ul>
 
+The wild-card can be used to specify the pattern.
 The list of supported DAS 
 <b>keys</b> can be found in 
 <b><a href="%s/services">Services</a></b> section.
@@ -35,10 +36,10 @@ For more details please read DAS
 
     card = """
 <h3 class="big">Help: DAS filters</h3>
-Do you know that DAS supports filters? Similar to UNIX you can apply
-<b>grep</b> filter to select certain data. For example,
-if you look for a given dataset=*RelVal*, you can select its number of events
-as simple as
+Do you know that DAS supports filters? You can apply
+<b>grep</b> filter to select part of the DAS record. For example,
+if you look for a given dataset pattern, e.g. dataset=*RelVal*, 
+you can select number of events as simple as
 <pre>
 dataset=*RelVal* | grep dataset.nevents
 </pre>
@@ -51,13 +52,13 @@ dataset=*RelVal* | grep dataset.name, dataset.nevents, dataset.nfiles
 
     card = """
 <h3 class="big">Help: DAS filters and conditions</h3>
-Do you know that DAS filters supports conditions. Their usage is trivial:
+Do you know that DAS filters supports conditions? Their usage is trivial:
 <pre>
 file dataset=/a/b/c | grep file.name, file.size>3000000, file.size<6000000
 </pre>
-The DAS filters supports: &lt;, &gt; and =. The usage of wild-card
-is allowed over string patterns. For example, you can select record
-attribute and apply wild-card condition at the same time:
+The DAS filters support: &lt;, &gt; and =. The usage of wild-card
+is allowed for string patterns. For example, you can select record
+attribute and apply a wild-card condition at the same time:
 <pre>
 dataset=*RelVal* | grep dataset.name, dataset.name=*RECO
 </pre>
@@ -79,7 +80,7 @@ Custom map-reduce function are also supported. Please contact DAS
 
     card = """
 <h3 class="big">Help: DAS date usage</h3>
-DAS has a special <b>date</b> keyword. It can accepts values either in
+DAS has special <b>date</b> keyword. It accepts values either in
 YYYYMMDD format or via <b>last</b> operator. For example:
 <pre>
 run date=20110316
@@ -95,7 +96,7 @@ Supported units for <b>last</b> operator are <b>d</b> (days),
 <h3 class="big">Help: DAS special keywords</h3>
 DAS has special <b>records</b> and <b>queries</b> keywords.
 The former can be used to look-up all records in DAS cache, while
-later shows most recent queries placed into DAS. The DAS filters
+later shows most recent queries placed in DAS. The DAS filters
 can be used with them too. For example, if you want to see if there
 are any file records in DAS cache you can simply type:
 <pre>
@@ -106,18 +107,17 @@ records | grep file.name
 
     card = """
 <h3 class="big">Help: DAS command line interface</h3>
-Do you know that you can use DAS from your terminal. Go to
+Do you know that you can use DAS from your terminal? Go to
 <b><a href="%s/cli">CLI</a></b> link and save it on your disk, e.g. as <em>das_cli</em>.
 Then you can use it as simple as
 <pre>
 python das_cli --query="dataset=/ExpressPhysics*"
 </pre>
-The default format is JSON. But you can apply filter and use plain data format, try
-the following:
+Please use <b>--help</b> for more options.
+The default output format is JSON. But you can apply filter and use plain data format, e.g.:
 <pre>
 python das_cli --query="dataset=/ExpressPhysics* | grep dataset.name, dataset.nevents" --format=plain
 /ExpressPhysics/Run2011A-Express-v2/FEVT 13321290
-/ExpressPhysics/Run2011A-Express-v1/FEVT 9654748
 ...
 </pre>
     """ % base
@@ -125,11 +125,12 @@ python das_cli --query="dataset=/ExpressPhysics* | grep dataset.name, dataset.ne
 
     card = """
 <h3 class="big">Help: DAS colors</h3>
-Do you know that colored little box, e.g.
+Colored little box, e.g.
 <cite style="background-color:%s;padding-left:7px; padding-right:7px">&nbsp;</cite>
 next to the Record output 
 represents specific CMS data-services.<br />
-Please follow DAS color map: <u>show</u> <u>hide</u>
+Please follow DAS color map: <u style="color:#8f1f08;">show</u>
+<u style="color:#8f1f08;">hide</u>
 links to find out which system it corresponds to.
 
 <br/>
