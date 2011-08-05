249a5b8be3120af750dbfa90665e00dc3db5c4ee
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2011-08-05 19:34:00 +0000

    Add utils/ddict.py (new DotDict module, forgot to include into previous patch with DotDict); add first implementation of reqmgr data-service and its map
    Signed-off-by: Valentin Kuznetsov <vkuznet@gmail.com>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@13438 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/python/DAS/services/reqmgr/reqmgr_service.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#-*- coding: ISO-8859-1 -*-
+
+"""
+ReqMgr service
+"""
+__author__ = "Valentin Kuznetsov"
+
+import re
+import time
+from DAS.services.abstract_service import DASAbstractService
+from DAS.utils.utils import map_validator, json_parser
+from DAS.utils.url_utils import getdata
+
+class ReqMgrService(DASAbstractService):
+    """
+    Helper class to provide DBS service
+    """
+    def __init__(self, config):
+        DASAbstractService.__init__(self, 'reqmgr', config)
+        self.map = self.dasmapping.servicemap(self.name)
+        map_validator(self.map)
+        entries = self.map.values()
+        self.ckey = entries[0].get('ckey')
+        self.cert = entries[0].get('cert')
+
+    def getdata(self, url, params, expire, headers=None, post=None):
+        """URL call wrapper"""
+        if  url[-1] == '/':
+            url = url[:-1]
+        for key, val in params.items():
+            url = '/'.join([url,params[key]])
+        params = {}
+        return getdata(url, params, headers, expire, post,
+                self.error_expire, self.verbose, self.ckey, self.cert)
+
+    def parser(self, query, dformat, source, api):
+        """
+        ReqMgr data-service parser.
+        """
+        if  api == 'inputdataset':
+            gen = DASAbstractService.parser(self, query, dformat, source, api)
+            for row in gen:
+                data = row['dataset']
+                data = data['WMCore.RequestManager.DataStructs.Request.Request']
+                yield data
