0e31cb0ea6c59291d128a8f6dbe4f78b5dfac5ce
valya <valya@4525493e-7705-40b1-a816-d608a930855b>
2011-11-18 18:48:49 +0000

    New unit tests for PrintManager
    Signed-off-by: Valentin Kuznetsov <vkuznet@gmail.com>
    
    
    git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/DAS/trunk@14592 4525493e-7705-40b1-a816-d608a930855b

--- test/logger_t.py
+++ test/logger_t.py
@@ -5,20 +5,69 @@
 unit test for logger module
 """
 
-import os
+import sys
 import unittest
-from DAS.utils.logger import PrintManager
+from cStringIO import StringIO
+from DAS.utils.logger import PrintManager, funcname
 
 class testDASLogger(unittest.TestCase):
     """
-    A test class for the DAS logger module
+    A test class for PrintManager
     """
     def setUp(self):
-        """
-        set up DAS core module
-        """
-        self.verbose = 0
-        self.logger = PrintManager('TestDASLogger', verbose=self.verbose)
+        "set up"
+        self.name = 'Logger'
+
+    def test_funcname(self):
+        "Test funcname"
+        result = funcname()
+        expect = 'test_funcname'
+        self.assertEqual(expect, result)
+        
+    def test_error(self):
+        "Test logger error method"
+        old_stdout = sys.stdout
+        logger = PrintManager(self.name) # verbose is irrelevant
+        sys.stdout = StringIO()
+        logger.error('test')
+        result = sys.stdout.getvalue()
+        expect = 'ERROR %s:%s test\n' % (self.name, funcname())
+        self.assertEqual(expect, result)
+        sys.stdout = old_stdout
+
+    def test_warning(self):
+        "Test logger warning method"
+        old_stdout = sys.stdout
+        logger = PrintManager(self.name) # verbose is irrelevant
+        sys.stdout = StringIO()
+        logger.warning('test')
+        result = sys.stdout.getvalue()
+        expect = 'WARNING %s:%s test\n' % (self.name, funcname())
+        self.assertEqual(expect, result)
+        sys.stdout = old_stdout
+
+    def test_info(self):
+        "Test logger info method"
+        old_stdout = sys.stdout
+        logger = PrintManager(self.name, verbose=1)
+        sys.stdout = StringIO()
+        logger.info('test')
+        result = sys.stdout.getvalue()
+        expect = 'INFO %s:%s test\n' % (self.name, funcname())
+        self.assertEqual(expect, result)
+        sys.stdout = old_stdout
+
+    def test_debug(self):
+        "Test logger debug method"
+        old_stdout = sys.stdout
+        logger = PrintManager(self.name, verbose=2)
+        sys.stdout = StringIO()
+        logger.debug('test')
+        result = sys.stdout.getvalue()
+        expect = 'DEBUG %s:%s test\n' % (self.name, funcname())
+        self.assertEqual(expect, result)
+        sys.stdout = old_stdout
+
 
 #
 # main
