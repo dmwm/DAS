9c4cce9acdbcfea152d950afdd914964e800dda4
Valentin Kuznetsov <vkuznet@gmail.com>
2012-12-05 15:40:27 -0500

    Return thread object in start_new_thread function; add dumpstacks function and register it with SIGQUIT signal

--- src/python/DAS/utils/thread.py
+++ src/python/DAS/utils/thread.py
@@ -8,6 +8,9 @@ Description: Wrapper around python threading/thread modules
 """
 
 # system modules
+import sys
+import signal
+import traceback
 import threading
 
 def set_thread_name(ident, name):
@@ -22,3 +25,21 @@ def start_new_thread(name, func, args):
     thr = threading.Thread(target=func, name=name, args=args)
     thr.daemon = True
     thr.start()
+    return thr
+
+def dumpstacks(signal, frame):
+    """
+    Dump context of all threads upon given signal
+    http://stackoverflow.com/questions/132058/showing-the-stack-trace-from-a-running-python-application
+    """
+    id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
+    code = []
+    for threadId, stack in sys._current_frames().items():
+        code.append("\n# Thread: %s(%d)" % (id2name.get(threadId,""), threadId))
+        for filename, lineno, name, line in traceback.extract_stack(stack):
+            code.append('File: "%s", line %d, in %s' % (filename, lineno, name))
+            if line:
+                code.append("  %s" % (line.strip()))
+    print "\n".join(code)
+
+signal.signal(signal.SIGQUIT, dumpstacks)
