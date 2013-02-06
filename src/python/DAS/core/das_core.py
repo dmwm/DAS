66d2d56c49a0187d56c18bb42f6a7797ea723271
Valentin Kuznetsov <vkuznet@gmail.com>
2013-02-06 17:13:55 -0500

    Add api=das_core to dasheader (to be recorded in DB)

--- src/python/DAS/core/das_core.py
+++ src/python/DAS/core/das_core.py
@@ -350,7 +350,7 @@ class DASCore(object):
         self.logger.info('services = %s' % services)
         das_timer('das_record', self.verbose)
         expire = 7*24*60*60 # 7 days, long enough to be overwriten by data-srv
-        header = dasheader("das", dasquery, expire)
+        header = dasheader("das", dasquery, expire, api='das_core')
         header['lookup_keys'] = []
         self.rawcache.insert_query_record(dasquery, header)
         das_timer('das_record', self.verbose)
