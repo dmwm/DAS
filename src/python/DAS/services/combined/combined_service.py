3a2e134de4b38223851c6d2fe231bc482e64b5ed
Valentin Kuznetsov <vkuznet@gmail.com>
2013-02-10 19:54:06 -0500

    Improve error message

--- src/python/DAS/services/combined/combined_service.py
+++ src/python/DAS/services/combined/combined_service.py
@@ -250,9 +250,8 @@ class CombinedService(DASAbstractService):
                 tstamp = dastimestamp('')
                 msg  = tstamp + ' Exception while processing DBS/Phedex info:'
                 msg += str(err)
-                row = {'site':{'name':'N/A', 'error':msg,
-                    'dataset_fraction':'N/A', 'block_fraction':'N/A',
-                    'block_completion':'N/A'}}
+                row = {'site':{'name':'Fail to look-up site info',
+                    'error':msg}, 'error': msg}
                 yield row
 
     def apicall(self, dasquery, url, api, args, dformat, expire):
