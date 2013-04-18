97076de09a8e85f252f6fb44cc3269b18be1d82c
Valentin Kuznetsov <vkuznet@gmail.com>
2013-04-18 17:20:11 -0400

    Disable for time being signal settings

--- src/python/DAS/utils/urlfetch_pycurl.py
+++ src/python/DAS/utils/urlfetch_pycurl.py
@@ -15,12 +15,14 @@ import pycurl
 
 # We should ignore SIGPIPE when using pycurl.NOSIGNAL - see
 # the libcurl tutorial for more info.
-try:
-    import signal
-    from signal import SIGPIPE, SIG_IGN
-    signal.signal(signal.SIGPIPE, signal.SIG_IGN)
-except ImportError:
-    pass
+# NOTE: I found that this cause a crash of DAS server, since signal works only
+# in main thread, disable for time being.
+#try:
+#    import signal
+#    from signal import SIGPIPE, SIG_IGN
+#    signal.signal(signal.SIGPIPE, signal.SIG_IGN)
+#except ImportError:
+#    pass
 
 try:
     import cStringIO as StringIO
