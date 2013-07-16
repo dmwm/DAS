572fd481f43a5b866a6ca769991fe765877652e5
Vid <vidmantas.zemleris@gmail.com>
2013-07-16 15:28:02 +0200

    basic waiting until DB is ready before bootstrap

--- src/python/DAS/tools/das_waitfordb.py
+++ src/python/DAS/tools/das_waitfordb.py
@@ -1,3 +1,6 @@
+from DAS.core.das_core import DASCore
+from DAS.core.das_mapping_db import DASMapping
+
 __author__ = 'vidma'
 
 import sys, time
@@ -5,34 +8,50 @@ from DAS.utils.das_config import das_readconfig
 from DAS.utils.das_db import db_connection, is_db_alive
 
 
-def on_success():
-    print 'DB is available'
-    sys.exit(0)
+def check_mappings_readiness():
+    config = das_readconfig()
+    # TODO: check if the mappings in DB are consistent
 
+    print('db alive. checking it\'s state...')
+    try:
+        #dascore = DASCore()
+        dasmapping = DASMapping(config)
+        if dasmapping.check_maps():
+            return True
+    except Exception, e:
+        print e
+
+    print 'no DAS mappings present...'
+    return False
+
+
+def on_db_available():
+    print 'DB is available'
+
+    if check_mappings_readiness():
+        print('Mappings seem to be fine...\n'
+              'just in case, wait 10s until the mappings are fully loaded...')
+        time.sleep(10)
+        sys.exit(0)
 
 def db_monitor(uri, func, sleep=5, max_retries=None):
     """
     Check status of MongoDB connection. Invoke provided function upon
     successfull connection.
     """
-    conn = db_connection(uri)
+    conn = None
     retries = 0
     while True:
-        if  not conn or not is_db_alive(uri):
+        # db is dead
+        if not (conn and is_db_alive(uri)):
             try:
                 conn = db_connection(uri)
-                if conn:
-                    func()
-
-                if  conn:
-                    print "### db_monitor re-established connection %s" % conn
-                else:
-                    print "### db_monitor, lost connection"
-
-            # sys.exit(0) is not caught by Exception
-            #  (but BaseException would catch it)
             except Exception, e:
                 print e
+
+        if conn and is_db_alive(uri):
+            print "### established connection %s" % conn
+            func()
 
         # limit the number of retries if needed
         retries += 1
@@ -42,17 +61,26 @@ def db_monitor(uri, func, sleep=5, max_retries=None):
         time.sleep(sleep)
 
 
-
-
 def waitfordb(max_time):
+    """
+    waits until DB is ready as well as until DAS mappings are created/updated.
+    """
     config = das_readconfig()
     dburi = config['mongodb']['dburi']
     sleep_time=5
-    db_monitor(dburi, on_success,
+    db_monitor(dburi, on_db_available,
                sleep=sleep_time, max_retries=max_time // sleep_time)
 
     print 'DB is not available and the timeout has passed'
     sys.exit(-1)
+
+def test():
+    # remove mappings
+    # shall fail
+
+    # stop db
+    # restart it - shall work
+    pass
 
 
 if __name__ == '__main__':
