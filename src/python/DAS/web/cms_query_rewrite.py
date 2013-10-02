1afc842d252efafd3bf92a9f2f142527f790bcc5
Vid <vidmantas.zemleris@gmail.com>
2013-10-02 12:46:08 +0200

    refactored convert2dasql to be inside cms_query_rewriter issues #65 and #77.

--- src/python/DAS/web/cms_query_rewrite.py
+++ src/python/DAS/web/cms_query_rewrite.py
@@ -5,6 +5,8 @@
 from pprint import pprint
 
 from DAS.core.das_query import DASQuery
+from DAS.core.das_ql import das_special_keys, das_record_keys
+
 from DAS.keywordsearch.metadata.schema_adapter_factory import getSchema
 from DAS.keywordsearch.tokenizer import get_keyword_without_operator as \
     get_filter_name
@@ -17,6 +19,7 @@ class CMSQueryRewrite(object):
     def __init__(self, cms_rep):
         self.cms_rep = cms_rep
         self.dasmgr = self.cms_rep.dasmgr
+        self.init_short_daskeys(self.dasmgr)
         # schema adapter from kws
         # TODO: get_field_list_for_entity_by_pk could be moved to DAS Core or...
         self.schema_adapter = getSchema(dascore=self.cms_rep.dasmgr)
@@ -58,7 +61,6 @@ class CMSQueryRewrite(object):
 
         # find all lookup (primary keys) for given das entity
         # It is safe to combine the queries
-        # TODO: add actual PK
         filters_first = [f for f in q_filters
                          if get_filter_name(f) in fields_avail]
         filters_nested = [f for f in q_filters
@@ -78,26 +80,27 @@ class CMSQueryRewrite(object):
                 DAS (or underlying services) do not support this query yet.
                 Still you can run multiple queries and combine their results:
 
-                    query: %(q1_str)s
-                    for each result:
-                        query: %(q2_str)s   (replacing <PK> with value of %(pk)s returned)
+                %(q1_str)s
+                for each result:
+                    %(q2_str)s  (replace <PK> with value of %(pk)s from first query)
 
-                Combination of results from the two queries will give you the results expected,
-                     except for aggregations which have to be implemented manually.
-                See documentation on <a href="%(cli)s">Command Line Interface</a>
-                ''' % {'q1_str': q1.convert2dasql(self.dasmgr),
-                       'q2_str': q2.convert2dasql(self.dasmgr),
+                Combination of the two queries will give the results expected,
+                except for aggregations which have to be implemented manually.
+
+                See documentation on Command Line Interface:
+                %(cli)s
+                ''' % {'q1_str': self.convert2dasql(q1),
+                       'q2_str': self.convert2dasql(q2),
                        'pk': pk,
                        'cli': 'https://cms-http-group.web.cern.ch/cms-http-group/apidoc/das/current/das_client.html'
                 }
-        #print msg
         return msg
 
     def check_fields(self, dasquery):
         '''
         run dataset=/ZMM/Summer11-DESIGN42_V11_428_SLHC1-v1/GEN-SIM | grep  run.bfield
         '''
-        DEBUG = True
+        DEBUG = False
 
         if not dasquery.filters:
             return
@@ -116,13 +119,12 @@ class CMSQueryRewrite(object):
             return
 
         if DEBUG:
-            pprint(['DASQUERY:', dasquery.mongo_query])
+            pprint(['DASQUERY:', dasquery.query])
             pprint(['RESULT ENTITY:', entity])
             pprint(['FILTERS FOR DAS QUERY:', dasquery.filters])
             pprint(['PARAMS FOR DAS QUERY:', dasquery.params()])
             pprint(['Feilds available in Current Query:', fields_available])
 
-        # TODO: here I may need to privide a particular PK
         pks_of_entity = list(set(self.cms_rep.dasmapping.mapkeys(entity)) \
                              & fields_available)
 
@@ -136,9 +138,52 @@ class CMSQueryRewrite(object):
             query_rewritable = q_fields_missing.issubset(
                 set(fields_in_nested_by_pk))
 
-            # if all fields that are still missing, are available in query='entity PK=pk'
+            # if all fields that are still missing,
+            #  are available in query='entity PK=pk'
             if query_rewritable and q_fields_missing:
                 print 'Rewrite OK'
                 return self._do_query_rewrite(dasquery, fields_available, pk)
 
         return False
+
+
+    def init_short_daskeys(self, dascore):
+        # we need to convert back into short das keys
+        #  (e.g. run.run_number->run)
+
+        # init field mappings
+        entity_names = {}
+        for das_key in dascore.das_keys(): # the short daskeys
+            long_daskeys = dascore.mapping.mapkeys(das_key)
+
+            for entity_long in long_daskeys:
+                entity_names[entity_long]  = das_key
+        self.entity_names = entity_names
+
+    short_daskey = lambda self, ldaskey: self.entity_names.get(ldaskey, ldaskey)
+
+    def convert2dasql(self, q):
+        """
+        transform DASQuery object back into DASQL (human readble form)
+        """
+        fields = q.mongo_query.get('fields', []) or []
+        exclude = das_record_keys()
+        lookups = (f for f in fields
+                   if f not in exclude)
+
+        das_conditions = q.mongo_query.get('spec', {}).iteritems()
+
+        dasql  = ','.join(lookups)
+        dasql += ' ' # space between lookup and conditions
+        dasql += ' '.join('%s=%s' % (self.short_daskey(k), v)
+                          for k, v in das_conditions)
+
+        # add post_filters
+        if q.filters:
+            dasql += ' | grep ' +', '.join(q.filters)
+
+
+        # not implemented:  language specials - between, date
+        #  (however these are not supported by keyword search neither)
+        # also aggregators (including unique) - left for user to implement
+        return dasql.strip()
