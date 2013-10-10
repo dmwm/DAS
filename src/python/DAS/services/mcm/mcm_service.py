658af0279d2e5ff8a598dc66092b61d895a91303
Valentin Kuznetsov <vkuznet@gmail.com>
2013-10-10 09:20:50 -0400

    Remove ReqMgr from MCM code; add local parser to drop unnecessary keys

--- src/python/DAS/services/mcm/mcm_service.py
+++ src/python/DAS/services/mcm/mcm_service.py
@@ -7,49 +7,10 @@ MCM data-service plugin.
 """
 __author__ = "Valentin Kuznetsov"
 
-# system modules
-import time
-
 # DAS modules
 from DAS.services.abstract_service import DASAbstractService
-from DAS.utils.utils import map_validator, get_key_cert
+from DAS.utils.utils import map_validator, json_parser
 from DAS.utils.url_utils import getdata
-from DAS.utils.global_scope import SERVICES
-from DAS.utils.urlfetch_pycurl import getdata as urlfetch_getdata
-
-import DAS.utils.jsonwrapper as json
-
-CKEY, CERT = get_key_cert()
-
-def mcm4dataset(url, reqmgr, dataset, expire):
-    "Get McM info for given dataset"
-    # get data from ReqMgr, we use reqmgr map and extract inputdataset URL
-    req_url = reqmgr.map['inputdataset']['url']
-    params  = {'dataset':dataset}
-    headers =  {'Accept': 'application/json' }
-    data, expire = reqmgr.getdata(req_url, params, expire, headers)
-    mcmurls = []
-
-    # parse ReqMgr data, collect PrepID and construct McM URLs
-    try:
-        rinfo = json.load(data)
-        for rec in rinfo:
-            key = 'WMCore.RequestManager.DataStructs.Request.Request'
-            prepid = rec[key].get('PrepID', None)
-            if  prepid:
-                mcmurls.append('%s/%s' % (url, prepid))
-    except Exception as exp:
-        error = 'Fail to parse ReqMgr output'
-        reason = 'data=%s, json.load error=%s' % (row, str(exp))
-        yield {'mcm':{'error':error, 'reason':reason}}
-
-    # contact McM data-service and get data for our list of McM URLs
-    headers = {}
-    gen = urlfetch_getdata(mcmurls, CKEY, CERT, headers)
-    for row in gen:
-        url = row['url']
-        data = json.loads(row['data'])
-        yield dict(mcm=data['results'])
 
 class MCMService(DASAbstractService):
     """
@@ -59,22 +20,6 @@ class MCMService(DASAbstractService):
         DASAbstractService.__init__(self, 'mcm', config)
         self.map = self.dasmapping.servicemap(self.name)
         map_validator(self.map)
-
-    def apicall(self, dasquery, url, api, args, dformat, expire):
-        "McM implementation of AbstractService:apicall method"
-#        if  api == 'mcm4dataset':
-#            time0   = time.time()
-#            reqmgr  = SERVICES.get('reqmgr', None) # reqmgr from global scope
-#            rawrows = mcm4dataset(url, reqmgr, args['dataset'], expire)
-#            dasrows = self.translator(api, rawrows)
-#            ctime   = time.time()-time0
-#            self.write_to_cache(dasquery, expire, url, api, args,
-#                    dasrows, ctime)
-#        else:
-#            super(MCMService, self).apicall(\
-#                    dasquery, url, api, args, dformat, expire)
-        super(MCMService, self).apicall(\
-                dasquery, url, api, args, dformat, expire)
 
     def getdata(self, url, params, expire, headers=None, post=None):
         """URL call wrapper"""
@@ -92,3 +37,19 @@ class MCMService(DASAbstractService):
                 self.error_expire, self.verbose, self.ckey, self.cert,
                 doseq=False, system=self.name)
         return result
+
+    def parser(self, dasquery, dformat, data, api):
+        "DAS parser for MCM data-service"
+        prim_key  = self.dasmapping.primary_key(self.name, api)
+        gen       = json_parser(data, self.logger)
+        counter   = 0
+        for rec in gen:
+            row = rec['results']
+            for key in ['_id', '_rev']:
+                if  key in row:
+                    del row[key]
+            yield {'mcm':row}
+            counter += 1
+        msg  = "api=%s, format=%s " % (api, dformat)
+        msg += "prim_key=%s yield %s rows" % (prim_key, counter)
+        self.logger.info(msg)
