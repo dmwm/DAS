bfbfb57ca080aecad022bc73c0f9d938f094a9a8
Vid <vidmantas.zemleris@gmail.com>
2013-12-03 15:41:03 +0100

    pylint/pep8 fixes for test/jsonwrapper_t.py

--- test/jsonwrapper_t.py
+++ test/jsonwrapper_t.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python
-#pylint: disable-msg=C0301,C0103
+#pylint: disable-msg=R0904,F0401
+# pylint disabled: R0904: applies to inherited module; F0401 - can not import
 
 """
 Unit test for jsonwrapper
 """
 
-
 import sys
 import unittest
 import traceback
-import urllib2, urllib
-import DAS.utils.jsonwrapper as myjson
+from DAS.utils import jsonwrapper as myjson
 
-class testUtils(unittest.TestCase):
+
+class TestUtils(unittest.TestCase):
     """
     A test class for the jsonwrapper module
     """
+
     def setUp(self):
         """
         set up DAS core module
         """
-        self.data = {'a':1, 'b':[{'c':1, 'd':['1','2']}]}
+        self.data = {'a': 1, 'b': [{'c': 1, 'd': ['1', '2']}]}
         self.module = myjson.MODULE
 
     @classmethod
@@ -52,36 +53,39 @@ class testUtils(unittest.TestCase):
             myjson.MODULE = self.module
 
     def _test_json(self):
-        """Test json wrapper"""
+        """ Test json wrapper """
         myjson.MODULE = 'json'
-        expect  = self.data
-        result  = myjson.loads(myjson.dumps(self.data))
+        expect = self.data
+        result = myjson.loads(myjson.dumps(self.data))
         self.assertEqual(expect, result)
 
-        expect  = self.data
-        result  = myjson.JSONDecoder().decode(myjson.JSONEncoder().encode(self.data))
+        expect = self.data
+        result = myjson.JSONDecoder().decode(
+            myjson.JSONEncoder().encode(self.data))
         self.assertEqual(expect, result)
 
-        data = {'a':1, 'b':2}
-        kwds = {'sort_keys':True}
+        data = {'a': 1, 'b': 2}
+        kwds = {'sort_keys': True}
         result1 = myjson.JSONEncoder(**kwds).encode(data)
-        data = {'b':2, 'a':1}
+        data = {'b': 2, 'a': 1}
         result2 = myjson.JSONEncoder(**kwds).encode(data)
         self.assertEqual(result1, result2)
 
         myjson.MODULE = self.module
 
     def _test_cjson(self):
-        """Test cjson wrapper"""
+        """ Test cjson wrapper """
         import cjson
+
         myjson.MODULE = 'cjson'
         myjson.cjson = cjson
-        expect  = self.data
-        result  = myjson.loads(myjson.dumps(self.data))
+        expect = self.data
+        result = myjson.loads(myjson.dumps(self.data))
         self.assertEqual(expect, result)
 
-        expect  = self.data
-        result  = myjson.JSONDecoder().decode(myjson.JSONEncoder().encode(self.data))
+        expect = self.data
+        result = myjson.JSONDecoder().decode(
+            myjson.JSONEncoder().encode(self.data))
         self.assertEqual(expect, result)
 
         myjson.MODULE = self.module
@@ -91,33 +95,34 @@ class testUtils(unittest.TestCase):
         import yajl
         myjson.MODULE = 'yajl'
         myjson.yajl = yajl  # so not to depend on import sequence
-        expect  = self.data
-        result  = myjson.loads(myjson.dumps(self.data))
+        expect = self.data
+        result = myjson.loads(myjson.dumps(self.data))
         self.assertEqual(expect, result)
 
-        expect  = self.data
-        result  = myjson.JSONDecoder().decode(myjson.JSONEncoder().encode(self.data))
+        expect = self.data
+        result = myjson.JSONDecoder().decode(
+            myjson.JSONEncoder().encode(self.data))
         self.assertEqual(expect, result)
 
-        data = {'a':1, 'b':2}
-        kwds = {'sort_keys':True}
+        data = {'a': 1, 'b': 2}
+        kwds = {'sort_keys': True}
         result1 = myjson.JSONEncoder(**kwds).encode(data)
-        data = {'b':2, 'a':1}
+        data = {'b': 2, 'a': 1}
         result2 = myjson.JSONEncoder(**kwds).encode(data)
         self.assertEqual(result1, result2)
 
         data = 123
         result1 = myjson.JSONEncoder(**kwds).encode(data)
-        kwds = {'sort_keys':True}
+        kwds = {'sort_keys': True}
         result2 = myjson.JSONEncoder(**kwds).encode(data)
         self.assertEqual(result1, result2)
 
-        data = {'a':123, 'b':[1, 2, 3], 'c':{'d':[1, '2', 3]}}
+        data = {'a': 123, 'b': [1, 2, 3], 'c': {'d': [1, '2', 3]}}
         result1 = myjson.JSONEncoder().encode(data)
         result2 = myjson.JSONDecoder().decode(result1)
         self.assertEqual(data, result2)
 
-        data = {'a':123, 'b':[1, 2, 3], 'c':{'d':[1, '2', 3]}}
+        data = {'a': 123, 'b': [1, 2, 3], 'c': {'d': [1, '2', 3]}}
         result1 = myjson.dumps(data)
         result2 = myjson.loads(result1)
         self.assertEqual(data, result2)
