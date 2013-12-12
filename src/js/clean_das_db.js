89da81eed672d2d2ab78c6847641a2d19b64efb2
Valentin Kuznetsov <vkuznet@gmail.com>
2013-12-12 13:43:24 -0500

    Add JS script to clean-up DAS databases

---
+++ src/js/clean_das_db.js
@@ -0,0 +1,4 @@
+parser = db.getSisterDB("parser");
+parser.db.drop();
+das = db.getSisterDB("das");
+das.dropDatabase();
---
+++ src/js/clean_mapping_db.js
@@ -0,0 +1,2 @@
+mapping = db.getSisterDB("mapping");
+mapping.dropDatabase();
