9a62ebb91c09c8ece8643c46835baa16e2ae2245
Valentin Kuznetsov <vkuznet@gmail.com>
2013-12-18 14:13:31 -0500

    Add Singleton implementation based on metaclass

--- src/python/DAS/utils/das_singleton.py
+++ src/python/DAS/utils/das_singleton.py
@@ -4,32 +4,13 @@
 """
 File: das_singleton.py
 Author: Valentin Kuznetsov <vkuznet@gmail.com>
-Description: DASCore singleton implementation
+Description: singleton implementation
+            http://stackoverflow.com/questions/6760685/creating-a-singleton-in-python
 """
 
-from DAS.core.das_core import DASCore
-from DAS.utils.utils import genkey
-
-class _DASMgrSingleton(object):
-    "DASCore Singleton class"
-    def __init__(self):
-        # just for the sake of information
-        self.instance = "Instance at %d" % self.__hash__()
-        self.params = {}
-
-    def create(self, **kwargs):
-        "Create DASCore object"
-        dashash = genkey(str(kwargs))
-        if  dashash in self.params:
-            return self.params[dashash]
-        else:
-            das = DASCore(**kwargs)
-            self.params[dashash] = das
-            return das
-
-DAS_MGR_SINGLETON = _DASMgrSingleton()
-
-def das_singleton(**kwargs):
-    "Return DASCore instance"
-    dasinst = DAS_MGR_SINGLETON.create(**kwargs)
-    return dasinst
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
