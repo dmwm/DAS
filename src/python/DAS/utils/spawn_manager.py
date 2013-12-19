061a373b6bc7f581cc1d5d7c603cd25e74cb74a9
Valentin Kuznetsov <vkuznet@gmail.com>
2013-12-19 12:06:12 -0500

    New module which introduces spawn functions and associated unit test (spawn functions are influenced by Erlang counterparts and implemented via multiprocessing/queue/pickle modules)

---
+++ src/python/DAS/utils/spawn_manager.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#pylint: disable-msg=R0903
+"""
+File       : spawn_manager.py
+Author     : Valentin Kuznetsov <vkuznet AT gmail dot com>
+Description: implementation of spawn function via multiprocessing module
+             it is based on Process/Queue pairing.
+"""
+
+# system modules
+import os
+import multiprocessing
+try:
+    import cPickle as pickle
+except:
+    import pickle
+
+def proc_worker(output, func, *args, **kwargs):
+    "Worker which runs given function and put results back to output queue"
+    pickled = kwargs.get('pickled', False)
+    if  'pickled' in kwargs:
+        del kwargs['pickled']
+    res = func(*args, **kwargs)
+    if  pickled:
+        output.put(pickle.dumps(res))
+    else:
+        output.put(res)
+
+def spawn(func, *args, **kwargs):
+    """
+    Implementation of spawn function via multiprocessing.Process module.
+    We initialize output queue, run given function via separate process,
+    and collect results from our output queue. The results should be
+    pickleable, e.g. simple types, lists, dicts.
+    """
+    kwargs['pickled'] = True
+    queue = multiprocessing.Queue()
+    name  = 'process_pid%s_ppid%s' % (os.getpid(), os.getppid())
+    proc  = multiprocessing.Process(target=proc_worker, name=name,
+                    args=tuple([queue, func] + list(args)),
+                    kwargs=kwargs)
+    proc.start()
+    proc.join()
+    return pickle.loads(queue.get())
+
+def spawn_queue(queue, func, *args, **kwargs):
+    """
+    Implementation of spawn function via multiprocessing.Process module.
+    We initialize output queue, run given function via separate process,
+    and collect results from our output queue. The results should be
+    pickleable, e.g. simple types, lists, dicts.
+    """
+    name  = 'process_pid%s_ppid%s' % (os.getpid(), os.getppid())
+    proc  = multiprocessing.Process(target=proc_worker, name=name,
+                    args=tuple([queue, func] + list(args)),
+                    kwargs=kwargs)
+    proc.start()
+    return proc
+
+class Process(object):
+    """Decorator to run a function through multiprocessing module"""
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        """
+        Run function through multiprocessing.Process, results are stored in
+        internal queue
+        """
+        return spawn(self.func, *args, **kwargs)
+
+class SpawnManager(object):
+    """
+    Class which utilizes spawn_queue function and provides API to run and
+    manage multiple processes
+    """
+    def __init__(self):
+        self.queue = multiprocessing.Queue()
+        self.procs = []
+
+    def spawn(self, func, *args, **kwargs):
+        "Spawn given function"
+        proc = spawn_queue(self.queue, func, *args, **kwargs)
+        self.procs.append(proc)
+
+    def join(self, debug=False):
+        "Join all processes and collect the results"
+        results = []
+        for proc in self.procs:
+            if  debug:
+                print proc.name
+            proc.join()
+            results.append(self.queue.get())
+        return results
---
+++ test/spawn_manager_t.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+#pylint: disable-msg=C0301,C0103
+
+"""
+Unit test for DAS spawn manager
+"""
+
+import os
+import time
+import unittest
+import multiprocessing
+
+from DAS.utils.spawn_manager import Process, spawn, spawn_queue, SpawnManager
+from DAS.utils.spawn_manager import spawn
+from DAS.core.das_ply import DASPLY
+
+@Process
+def test_int(val):
+    return val
+
+@Process
+def test_list(val):
+    return [val, val]
+
+@Process
+def test_dict(val):
+    return {'val': val, 'array': [val, val]}
+
+def test_wait(val):
+    print "test_wait, time=%s, PID=%s, PPID=%s" \
+            % (time.time(), os.getpid(), os.getppid())
+    time.sleep(val)
+    return val
+
+class MyTest(object):
+    def __init__(self, val):
+        self.val = val
+    def method(self, val):
+        return {'self.val': self.val, 'val': val}
+
+class testUtils(unittest.TestCase):
+    """
+    A test class for the DAS task manager class
+    """
+    def setUp(self):
+        """
+        set up shared array of type int with given size
+        """
+        pass
+
+    def test_spawn_functions(self):
+        """Test spawn functions"""
+        val = 1
+        result = test_int(val)
+        expect = val
+        self.assertEqual(result, expect)
+
+        result = test_list(val)
+        expect = [val, val]
+        self.assertEqual(result, expect)
+
+        result = test_dict(val)
+        expect = {'val': val, 'array': [val, val]}
+        self.assertEqual(result, expect)
+
+    def test_spawn_class(self):
+        """Test spawn class methods"""
+        val = 1
+        obj = MyTest(val)
+        result = spawn(obj.method, val+val)
+        expect = {'self.val': val, 'val': val+val}
+        self.assertEqual(result, expect)
+
+    def test_spawn_queue(self):
+        """Test spawn_queue function"""
+        print "\n### test_spawn_queue"
+        queue  = multiprocessing.Queue()
+        sleep  = 2
+        # spawn 3 processes which will sleep for given time
+        time1  = time.time()
+        spawn_queue(queue, test_wait, sleep)
+        time1  = time.time()-time1
+        time2  = time.time()
+        spawn_queue(queue, test_wait, sleep)
+        time2  = time.time()-time2
+        time3  = time.time()
+        spawn_queue(queue, test_wait, sleep)
+        time3  = time.time()-time3
+        # get results from result queue
+        res1   = queue.get()
+        res2   = queue.get()
+        res3   = queue.get()
+        # compare max diff time between spawned processes
+#        print "times", time1, time2, time3
+        max_t  = max(abs(time1-time2), abs(time1-time3), abs(time2-time3))
+        result = True if max_t < sleep else False
+        expect = True
+        self.assertEqual(result, expect)
+        # check that returned results are consistent
+        self.assertEqual(res1, sleep)
+        self.assertEqual(res2, sleep)
+        self.assertEqual(res3, sleep)
+
+    def test_spawn_manager(self):
+        """Test spawn_queue function"""
+        print "\n### test_spawn_manager"
+        mgr = SpawnManager()
+        sleep  = 2
+        # spawn 3 processes which will sleep for given time
+        time1  = time.time()
+        mgr.spawn(test_wait, sleep)
+        time1  = time.time()-time1
+        time2  = time.time()
+        mgr.spawn(test_wait, sleep)
+        time2  = time.time()-time2
+        time3  = time.time()
+        mgr.spawn(test_wait, sleep)
+        time3  = time.time()-time3
+        # get results from result queue
+        result = mgr.join()
+        # check that returned results are consistent
+        self.assertEqual(result, [sleep, sleep, sleep])
+        # compare max diff time between spawned processes
+        max_t  = max(abs(time1-time2), abs(time1-time3), abs(time2-time3))
+        result = True if max_t < sleep else False
+        expect = True
+        self.assertEqual(result, expect)
+
+    def test_spawn_manager(self):
+        """Test spawn_queue function"""
+        dasservices = ['dbs', 'dbs3']
+        daskeys = ['dataset']
+        parserdir = os.getcwd()
+        query="dataset=/ZMM*/*/*"
+        dasply = DASPLY(parserdir, daskeys, dasservices, verbose=0)
+        dasply.build()
+        ply_q1 = dasply.parser.parse(query)
+        ply_q2 = spawn(dasply.parser.parse, query)
+        self.assertEqual(ply_q1, ply_q2)
+#
+# main
+#
+if __name__ == '__main__':
+    unittest.main()
