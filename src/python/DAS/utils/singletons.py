42222cd5b90f2223ba806d95264d404fea402958
Valentin Kuznetsov <vkuznet@gmail.com>
2013-12-23 10:53:31 -0500

    Adjust code to pass pylint score test

--- src/python/DAS/utils/singletons.py
+++ src/python/DAS/utils/singletons.py
@@ -9,10 +9,12 @@ Description: singleton implementation
 """
 
 class Singleton(type):
+    """Implementation of Singleton class"""
     _instances = {}
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = \
+                    super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 # Examples
