7202c0702bc7e5ee95b9b76e01a174ec9bcba4b5
Vid <vidmantas.zemleris@gmail.com>
2014-01-24 18:51:53 +0100

    initial travis support

---
+++ init_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DAS_ROOT=`python -c "import os, DAS; print os.path.dirname(DAS.__file__)"`
+export DAS_CONFIG=$DAS_ROOT/etc/das.cfg
+export DAS_JSPATH=$DAS_ROOT/web/js
+export DAS_CSSPATH=$DAS_ROOT/web/css
+export DAS_IMAGESPATH=$DAS_ROOT/web/images
+# TODO: YUI is not yet installed automatically
+export YUI_ROOT=$DAS_ROOT/web/js
