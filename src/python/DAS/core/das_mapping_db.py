c504d6efabbe2cf14c78b1a1cf519c60b951a91e
Valentin Kuznetsov <vkuznet@gmail.com>
2014-03-15 20:59:15 -0400

    Fix issues with single key=val query, e.g. tier=*, type=*

--- src/python/DAS/core/das_mapping_db.py
+++ src/python/DAS/core/das_mapping_db.py
@@ -673,10 +673,14 @@ class DASMapping(object):
             return False
         ikeys = [k.split('.')[0] for k in icond.keys()]
         dkeys = []
+        params = entry.get('params', {})
         for row in entry.get('das_map', []):
             if  'api_arg' in row:
                 das_key = row['das_key']
-                dkeys.append(das_key)
+                if  params.get(das_key, '') != 'optional':
+                    dkeys.append(das_key)
+            else:
+                dkeys.append(row['das_key'])
         if  set(ikeys) & set(dkeys) == set(ikeys):
             return True
         return False
@@ -703,6 +707,8 @@ class DASMapping(object):
                 else:
                     if  api_param not in names:
                         names.append(api_param)
+            else:
+                names.append(row['rec_key'])
         return names
 
     def notations(self, system=None):
