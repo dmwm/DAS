ab331c74a9f082dc300c24e4563ea93c5c618cde
Valentin Kuznetsov <vkuznet@gmail.com>
2014-05-02 14:47:21 -0400

    Remove runrange from dbs3 test since it is no longer in use

--- test/dbs3_t.py
+++ test/dbs3_t.py
@@ -6,34 +6,16 @@ Unit test for DAS DBS module
 """
 
 import unittest
-from DAS.services.dbs3.dbs3_service import runrange
 
 class testDBS(unittest.TestCase):
     """
     A test class for the DAS DBS3 module
     """
 
-    def test_runrange(self): 
-        """test runrange function"""
-        result = runrange(123456, 234567)
-        expect = '[123456,234567]'
-        self.assertEqual(expect, result)
-
-        result = runrange(234567, 123456)
-        expect = '[123456,234567]'
-        self.assertEqual(expect, result)
-
-        result = runrange(123456, 123456)
-        expect = "123456"
-        self.assertEqual(expect, result)
-
-        result = runrange(123456, 234567, True)
-        expect = "['123456-234567']"
-        self.assertEqual(expect, result)
-
-        result = runrange(123456, 123456, True)
-        expect = "123456"
-        self.assertEqual(expect, result)
+    def test_dbs3(self): 
+        """test dbs3 function"""
+#        self.assertEqual(expect, result)
+        pass
 
 #
 # main
