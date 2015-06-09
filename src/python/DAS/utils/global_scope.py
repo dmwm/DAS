be4f8e04b31eb6465762004b9896a5917b3d0192
Valentin Kuznetsov <vkuznet@gmail.com>
2015-06-09 17:14:37 -0400

    Remove global scope module

--- src/python/DAS/utils/global_scope.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: ISO-8859-1 -*-
-"""
-File       : global_scope.py
-Author     : Valentin Kuznetsov <vkuznet@gmail.com>
-Description: Keep track of global scope variables
-"""
-
-SERVICES = {} # DAS services
